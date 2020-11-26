790065f0082465d10bbcbe12558d99d8a78ca470
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2020-11-26 16:33:52 +0100

    epochs from ParseArgs

--- modeling/rnn_tf/Train.py
+++ modeling/rnn_tf/Train.py
@@ -105,7 +105,7 @@ def train_network():
 
     history = net.fit(
         train_set,
-        epochs=args.epochs,
+        epochs=args.num_epochs,
         verbose=1,
         shuffle=False,
         validation_data=test_set,
