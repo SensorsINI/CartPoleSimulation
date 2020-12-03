1fffa8277bc503b5813500224afd16df156556d9
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2020-12-04 00:45:51 +0100

    Corrected false comment

--- src/globals.py
+++ src/globals.py
@@ -81,7 +81,7 @@ k_globals = 4.0 / 3.0  # Dimensionless factor of moment of inertia of the pole
 # (I = k*m*L^2) (with L being half if the length)
 
 # Variables for random trace generation
-# Complexity of the random trace, number of random points used for interpolation (not accounting starting point)
+# Complexity of the random trace, number of random points used for interpolation
 track_relative_complexity_globals = 0.3
 random_length_globals = 1e1  # Number of seconds in the random length trace
 interpolation_type_globals = 'previous'  # Sets how to interpolate between turning points of random trace
