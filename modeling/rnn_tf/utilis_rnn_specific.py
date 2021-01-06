5c867c064faae11c4aed9786e358854d6802b920
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2021-01-06 20:15:52 +0100

    Plotting with and without motorpower

--- modeling/rnn_tf/utilis_rnn_specific.py
+++ modeling/rnn_tf/utilis_rnn_specific.py
@@ -14,6 +14,8 @@ def plot_results_specific(targets_pd, rnn_outputs, features_pd_denorm, time_axis
         time_axis_string = 'Time [s]'
 
     figs = []
+    angle_target = None
+    angle_output = None
 
     if ('s.position' in targets_pd) and ('s.position' in rnn_outputs):
         if ('s.angle' in targets_pd) and ('s.angle' in rnn_outputs):
@@ -27,10 +29,17 @@ def plot_results_specific(targets_pd, rnn_outputs, features_pd_denorm, time_axis
         position_target = targets_pd['s.position'].to_numpy()
         position_output = rnn_outputs['s.position'].to_numpy()
 
-        Q = features_pd_denorm['Q'].to_numpy()
+        number_of_plots = 1
+        if angle_output is not None:
+            number_of_plots += 1
+            if 'Q' in features_pd_denorm.columns:
+                number_of_plots += 1
+
+
+
 
         # Create a figure instance
-        fig, axs = plt.subplots(3, 1, figsize=(18, 10), sharex=True)
+        fig, axs = plt.subplots(number_of_plots, 1, figsize=(18, 10), sharex=True)
         plt.subplots_adjust(hspace=0.4)
         start_idx = 0
         axs[0].set_title(comment, fontsize=20)
@@ -49,44 +58,49 @@ def plot_results_specific(targets_pd, rnn_outputs, features_pd_denorm, time_axis
 
         axs[0].legend()
 
+        if number_of_plots>1:
+            axs[1].set_ylabel("Angle (deg)", fontsize=18)
+            axs[1].plot(time_axis, angle_target, 'k:', markersize=12, label='Ground Truth')
+            axs[1].plot(time_axis, angle_output, 'b', markersize=12,
+                        label='Predicted angle')
 
-        axs[1].set_ylabel("Angle (deg)", fontsize=18)
-        axs[1].plot(time_axis, angle_target, 'k:', markersize=12, label='Ground Truth')
-        axs[1].plot(time_axis, angle_output, 'b', markersize=12,
-                    label='Predicted angle')
+            axs[1].plot(time_axis[start_idx], angle_target[start_idx], 'g.', markersize=16,
+                        label='Start')
+            axs[1].plot(time_axis[start_idx], angle_output[start_idx], 'g.', markersize=16)
+            axs[1].plot(time_axis[-1], angle_target[-1], 'r.', markersize=16, label='End')
+            axs[1].plot(time_axis[-1], angle_output[-1], 'r.', markersize=16)
+            if closed_loop_enabled:
+                axs[1].plot(time_axis[close_loop_idx], angle_target[close_loop_idx], '.',
+                            color='darkorange', markersize=16, label='connect output->input')
+                axs[1].plot(time_axis[close_loop_idx], angle_output[close_loop_idx], '.',
+                            color='darkorange', markersize=16)
+            axs[1].legend()
 
-        axs[1].plot(time_axis[start_idx], angle_target[start_idx], 'g.', markersize=16,
-                    label='Start')
-        axs[1].plot(time_axis[start_idx], angle_output[start_idx], 'g.', markersize=16)
-        axs[1].plot(time_axis[-1], angle_target[-1], 'r.', markersize=16, label='End')
-        axs[1].plot(time_axis[-1], angle_output[-1], 'r.', markersize=16)
-        if closed_loop_enabled:
-            axs[1].plot(time_axis[close_loop_idx], angle_target[close_loop_idx], '.',
-                        color='darkorange', markersize=16, label='connect output->input')
-            axs[1].plot(time_axis[close_loop_idx], angle_output[close_loop_idx], '.',
-                        color='darkorange', markersize=16)
-        axs[1].legend()
+        if number_of_plots>2:
+            Q = features_pd_denorm['Q'].to_numpy()
+            axs[2].set_ylabel("Motor (-)", fontsize=18)
+            axs[2].plot(time_axis, Q, 'r', markersize=12, label='Ground Truth')
 
+            axs[2].plot(time_axis[start_idx], Q[start_idx], 'g.', markersize=16,
+                        label='Start')
+            axs[2].plot(time_axis[-1], Q[-1], 'r.', markersize=16, label='End')
+            if closed_loop_enabled:
+                axs[2].plot(time_axis[close_loop_idx], Q[close_loop_idx], '.',
+                            color='darkorange', markersize=16, label='connect output->input')
 
-        axs[2].set_ylabel("Motor (-)", fontsize=18)
-        axs[2].plot(time_axis, Q, 'r', markersize=12, label='Ground Truth')
+            axs[2].tick_params(axis='both', which='major', labelsize=16)
 
-        axs[2].plot(time_axis[start_idx], Q[start_idx], 'g.', markersize=16,
-                    label='Start')
-        axs[2].plot(time_axis[-1], Q[-1], 'r.', markersize=16, label='End')
-        if closed_loop_enabled:
-            axs[2].plot(time_axis[close_loop_idx], Q[close_loop_idx], '.',
-                        color='darkorange', markersize=16, label='connect output->input')
-
-        axs[2].tick_params(axis='both', which='major', labelsize=16)
-
-        axs[2].set_xlabel(time_axis_string, fontsize=18)
-        axs[2].legend()
+            axs[2].set_xlabel(time_axis_string, fontsize=18)
+            axs[2].legend()
 
         figs.append(fig)
 
     if ('s.positionD' in targets_pd) and ('s.positionD' in rnn_outputs) and \
             ('s.angleD' in targets_pd) and ('s.angleD' in rnn_outputs):
+
+        number_of_plots = 2
+        if 'Q' in features_pd_denorm.columns:
+            number_of_plots += 1
 
         positionD_target = targets_pd['s.positionD'].to_numpy()
         positionD_output = rnn_outputs['s.positionD'].to_numpy()
@@ -94,10 +108,8 @@ def plot_results_specific(targets_pd, rnn_outputs, features_pd_denorm, time_axis
         angleD_target = targets_pd['s.angleD'].to_numpy()
         angleD_output = rnn_outputs['s.angleD'].to_numpy()
 
-        Q = features_pd_denorm['Q'].to_numpy()
-
         # Create a figure instance
-        fig, axs = plt.subplots(3, 1, figsize=(18, 10), sharex=True)
+        fig, axs = plt.subplots(number_of_plots, 1, figsize=(18, 10), sharex=True)
         plt.subplots_adjust(hspace=0.4)
         start_idx = 0
         axs[0].set_title(comment, fontsize=20)
@@ -134,21 +146,22 @@ def plot_results_specific(targets_pd, rnn_outputs, features_pd_denorm, time_axis
                         color='darkorange', markersize=16)
         axs[1].legend()
 
+        if number_of_plots>2:
+            Q = features_pd_denorm['Q'].to_numpy()
+            axs[2].set_ylabel("Motor (-)", fontsize=18)
+            axs[2].plot(time_axis, Q, 'r', markersize=12, label='Ground Truth')
 
-        axs[2].set_ylabel("Motor (-)", fontsize=18)
-        axs[2].plot(time_axis, Q, 'r', markersize=12, label='Ground Truth')
+            axs[2].plot(time_axis[start_idx], Q[start_idx], 'g.', markersize=16,
+                        label='Start')
+            axs[2].plot(time_axis[-1], Q[-1], 'r.', markersize=16, label='End')
+            if closed_loop_enabled:
+                axs[2].plot(time_axis[close_loop_idx], Q[close_loop_idx], '.',
+                            color='darkorange', markersize=16, label='connect output->input')
 
-        axs[2].plot(time_axis[start_idx], Q[start_idx], 'g.', markersize=16,
-                    label='Start')
-        axs[2].plot(time_axis[-1], Q[-1], 'r.', markersize=16, label='End')
-        if closed_loop_enabled:
-            axs[2].plot(time_axis[close_loop_idx], Q[close_loop_idx], '.',
-                        color='darkorange', markersize=16, label='connect output->input')
+            axs[2].tick_params(axis='both', which='major', labelsize=16)
 
-        axs[2].tick_params(axis='both', which='major', labelsize=16)
-
-        axs[2].set_xlabel(time_axis_string, fontsize=18)
-        axs[2].legend()
+            axs[2].set_xlabel(time_axis_string, fontsize=18)
+            axs[2].legend()
 
         figs.append(fig)
 
