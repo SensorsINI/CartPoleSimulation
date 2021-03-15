2beaeeacfd096a235b5376274604c0745c2e2182
heetmeyer <heerfn@yahoo.de>
2021-03-15 15:31:20 +0100

    Comment out test functions

--- CartPole/_CartPole_mathematical_helpers.py
+++ CartPole/_CartPole_mathematical_helpers.py
@@ -70,16 +70,16 @@ def cartpole_state_vector_to_namespace(s_vector: np.ndarray) -> SimpleNamespace:
     return s_namespace
 
 
-# Test functions
-s = create_cartpole_state(dict(angleD=12.1, angleDD=-33.5, position=2.3, positionD=-19.77, positionDD=3.42))
-s[cartpole_state_varname_to_index('positionD')] = -14.9
-cartpole_state_index_to_varname(4)
+# # Test functions
+# s = create_cartpole_state(dict(angleD=12.1, angleDD=-33.5, position=2.3, positionD=-19.77, positionDD=3.42))
+# s[cartpole_state_varname_to_index('positionD')] = -14.9
+# cartpole_state_index_to_varname(4)
 
-sn = SimpleNamespace()
-sn.position=23.55
-sn.angleDD=4.11
-sn.eew = -1.22
-q = cartpole_state_namespace_to_vector(sn)
-v = cartpole_state_vector_to_namespace(q)
+# sn = SimpleNamespace()
+# sn.position=23.55
+# sn.angleDD=4.11
+# sn.eew = -1.22
+# q = cartpole_state_namespace_to_vector(sn)
+# v = cartpole_state_vector_to_namespace(q)
 
-print(s)
+# print(s)
