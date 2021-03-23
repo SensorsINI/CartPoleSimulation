161af45dfd93e576d9a0696a69e1d87894edc339
heetmeyer <heerfn@yahoo.de>
2021-03-23 22:54:24 +0100

    Add mode for logging all cost components

--- Controllers/controller_mppi.py
+++ Controllers/controller_mppi.py
@@ -30,8 +30,8 @@ from copy import deepcopy
 """Timestep and sampling settings"""
 dt = 0.02  # s
 mpc_horizon = 1.0
-mpc_samples = int(mpc_horizon / dt)
-mc_samples = 4000
+mpc_samples = int(mpc_horizon / dt)  # Number of steps in MPC horizon
+mc_samples = 4000  # Number of Monte Carlo samples
 
 
 """Define indices of values in state statically"""
@@ -43,7 +43,7 @@ POSITIOND_IDX = cartpole_state_varname_to_index("positionD").item()
 
 """MPPI constants"""
 R = 1.0e0  # How much to punish Q
-LBD = 10  # cost parameter lambda
+LBD = 10  # Cost parameter lambda
 NU = 1.0e1  # Exploration variance
 
 
@@ -52,15 +52,10 @@ parallelize = True
 _cartpole_ode = conditional_decorator(jit(nopython=True), parallelize)(_cartpole_ode)
 
 
-"""Init logging lists"""
-distance_differences, E_pots, E_kins_pole, E_kins_cart, control_costs = (
-    np.array([]),
-    np.array([]),
-    np.array([]),
-    np.array([]),
-    np.array([]),
-)
-DEBUG = True
+"""Init logging variables"""
+DEBUG = False
+# Save average cost for each cost component
+COST_LOGS = []
 
 
 """Cost function helpers"""
@@ -94,13 +89,42 @@ def trajectory_rollouts(s, S_tilde_k, u, delta_u, target_position):
         s_horizon[k, 0, :] = s
         for i in range(1, mpc_samples):
             s_last = s_horizon[k, i - 1, :]
+            # Explicit Euler integration step
             derivatives = motion_derivatives(s_last, u[i] + delta_u[k, i])
             s_next = s_last + derivatives * dt
             s_horizon[k, i, :] = s_next
 
-            S_tilde_k[k] += q(s_next, u[i], delta_u[k, i], target_position)
+            cost_increment, _, _, _, _, _ = q(
+                s_next, u[i], delta_u[k, i], target_position
+            )
+            S_tilde_k[k] += cost_increment
 
-    return S_tilde_k
+    return S_tilde_k, None
+
+
+@conditional_decorator(jit(nopython=True), parallelize)
+def trajectory_rollouts_debug(s, S_tilde_k, u, delta_u, target_position):
+    s_horizon = np.zeros((mc_samples, mpc_samples, s.size))
+    cost_logs_internal = np.zeros((mc_samples, 5, mpc_samples))
+    for k in range(mc_samples):
+        s_horizon[k, 0, :] = s
+        for i in range(1, mpc_samples):
+            s_last = s_horizon[k, i - 1, :]
+            # Explicit Euler integration step
+            derivatives = motion_derivatives(s_last, u[i] + delta_u[k, i])
+            s_next = s_last + derivatives * dt
+            s_horizon[k, i, :] = s_next
+
+            cost_increment, dd, ep, ekp, ekc, cc = q(
+                s_next, u[i], delta_u[k, i], target_position
+            )
+            S_tilde_k[k] += cost_increment
+            cost_logs_internal[k, :, i] = [dd, ep, ekp, ekc, cc]
+
+    return S_tilde_k, cost_logs_internal
+
+
+rollout_function = trajectory_rollouts_debug if DEBUG else trajectory_rollouts
 
 
 @conditional_decorator(jit(nopython=True), parallelize)
@@ -119,18 +143,19 @@ def motion_derivatives(s: np.ndarray, u: float):
 def q(s, u, delta_u, target_position):
     """Cost function per iteration"""
     dd = distance_difference(s, target_position) * 1.0e4
-    ep = 500 * E_pot_cost(s)
+    ep = 100 * E_pot_cost(s)
     ekp = E_kin_pol(s)
     ekc = 100 * E_kin_cart(s)
     cc = (
         0.5 * (1 - 1.0 / NU) * R * (delta_u ** 2) + R * u * delta_u + 0.5 * R * (u ** 2)
     )
 
-    if np.abs(u + delta_u) > 1.0:
-        return 1.0e5
-
     q = dd + ep + ekp + ekc + cc
-    return q
+
+    if np.abs(u + delta_u) > 1.0:
+        q = 1.0e5
+
+    return q, dd, ep, ekp, ekc, cc
 
 
 @conditional_decorator(jit(nopython=True), parallelize)
@@ -153,9 +178,6 @@ def update_inputs(u: np.ndarray, S: np.ndarray, delta_u: np.ndarray):
     :return: Input u for the whole MPC horizon updated with reward-weighted control perturbations
     """
     for i in range(mpc_samples):
-        # self.u[i] += reward_weighted_average(
-        #     self.S_tilde[:, -1] - self.S_tilde[:, i], self.delta_u[:, i]
-        # )
         u[i] += reward_weighted_average(S, delta_u[:, i])
     return u
 
@@ -171,12 +193,12 @@ class controller_mppi(template_controller):
 
         self.rho_sqrt_inv = 0.01
         self.avg_cost = []
-        self.num_timesteps = 0
+
+        self.iteration = 0
 
         self.s_horizon = np.zeros(())
         self.u = np.zeros((mpc_samples), dtype=float)
         self.delta_u = np.zeros((mc_samples, mpc_samples), dtype=float)
-        self.S_tilde = np.zeros((mc_samples, mpc_samples), dtype=float)
         self.S_tilde_k = np.zeros((mc_samples), dtype=float)
 
     def initialize_perturbations(
@@ -210,22 +232,23 @@ class controller_mppi(template_controller):
         self.s = s
         self.target_position = target_position.item()
 
-        self.num_timesteps += 1
+        self.iteration += 1
 
         # Initialize perturbations and cost arrays
         # self.delta_u = self.initialize_perturbations(
         #     stdev=self.rho_sqrt_inv / np.sqrt(dt), random_walk=False
         # )  # N(mean=0, var=1/(rho*dt))
         self.delta_u = self.initialize_perturbations(stdev=0.2)
-        self.S_tilde = np.zeros_like(self.S_tilde)
         self.S_tilde_k = np.zeros_like(self.S_tilde_k)
 
         # Run parallel trajectory rollouts for different input perturbations
-        self.S_tilde_k = trajectory_rollouts(
+        self.S_tilde_k, cost_logs_internal = rollout_function(
             self.s, self.S_tilde_k, self.u, self.delta_u, self.target_position,
         )
 
-        self.avg_cost.append(np.mean(self.S_tilde_k, axis=0))
+        if DEBUG:
+            self.avg_cost.append(np.mean(self.S_tilde_k, axis=0))
+            COST_LOGS.append(np.mean(cost_logs_internal, axis=0))
 
         # Update inputs with weighted perturbations
         self.u = update_inputs(self.u, self.S_tilde_k, self.delta_u)
@@ -240,48 +263,26 @@ class controller_mppi(template_controller):
         return Q  # normed control input in the range [-1,1]
 
     def controller_report(self):
-        # Graph the average state cost per iteration
-        time_axis = dt * np.arange(start=0, stop=len(self.avg_cost))
-        plt.figure(num=2, figsize=(8, 8))
-        plt.plot(time_axis, self.avg_cost)
-        plt.ylabel("avg_cost")
-        plt.xlabel("time")
-        plt.title("Cost over iterations")
-        plt.show()
-
         if DEBUG:
-            self.num_timesteps -= 1
+            # Graph the average state cost per iteration
+            time_axis = dt * np.arange(start=0, stop=len(self.avg_cost))
+            plt.figure(num=2, figsize=(8, 8))
+            plt.plot(time_axis, self.avg_cost)
+            plt.ylabel("avg_cost")
+            plt.xlabel("time")
+            plt.title("Cost over iterations")
+            plt.show()
 
-            distance_differences = np.reshape(
-                np.array(self.distance_differences[mc_samples * (mpc_samples - 1) :]),
-                (self.num_timesteps, mc_samples, mpc_samples - 1),
-            )
-            E_pots = np.reshape(
-                np.array(self.E_pots[mc_samples * (mpc_samples - 1) :]),
-                (self.num_timesteps, mc_samples, mpc_samples - 1),
-            )
-            E_kins_pole = np.reshape(
-                np.array(self.E_kins_pole[mc_samples * (mpc_samples - 1) :]),
-                (self.num_timesteps, mc_samples, mpc_samples - 1),
-            )
-            E_kins_cart = np.reshape(
-                np.array(self.E_kins_cart[mc_samples * (mpc_samples - 1) :]),
-                (self.num_timesteps, mc_samples, mpc_samples - 1),
-            )
-
+            clgs = np.stack(COST_LOGS, axis=0)  # ITERATIONS x 5 x mpc_steps
             plt.figure(num=3, figsize=(12, 8))
             plt.plot(
-                np.mean(self.distance_differences[:, :, -1], axis=1),
-                label="Distance difference cost",
+                clgs[:, 0, 1], label="Distance difference cost",
             )
-            plt.plot(np.mean(self.E_pots[:, :, -1], axis=1), label="E_pot cost")
-            plt.plot(
-                np.mean(self.E_kins_pole[:, :, -1], axis=1), label="E_kin_pole cost"
-            )
-            plt.plot(
-                np.mean(self.E_kins_cart[:, :, -1], axis=1), label="E_kin_cart cost"
-            )
-            plt.title("Cost components over time")
+            plt.plot(clgs[:, 1, 1], label="E_pot cost")
+            plt.plot(clgs[:, 2, 1], label="E_kin_pole cost")
+            plt.plot(clgs[:, 3, 1], label="E_kin_cart cost")
+            plt.plot(clgs[:, 4, 1], label="Control cost")
+            plt.title("Cost of each first MPC horizon step over time")
             plt.legend()
             plt.show()
 
