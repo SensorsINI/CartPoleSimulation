Corrected names for saving and new cost function for do-mpc IDEA: add 20% randomness to each factor of cost function

CartPole/_CartPole_mathematical_helpers.py
@@ -23,7 +23,7 @@ def wrap_angle_rad(angle):
 
 STATE_VARIABLES = np.sort(['angle', 'angleD', 'angleDD', 'position', 'positionD', 'positionDD', 'angle_cos', 'angle_sin'])
 
-def create_cartpole_state(state: dict={}) -> np.ndarray:
+def create_cartpole_state(state: dict={}, dtype=None) -> np.ndarray:
     """
     Constructor of cartpole state from named arguments. The order of variables is fixed in STATE_VARIABLES.
 
@@ -42,7 +42,10 @@ def create_cartpole_state(state: dict={}) -> np.ndarray:
     state['angle_cos'] = np.cos(state['angle']) if 'angle' in state.keys() else np.cos(0.0)
     state['angle_sin'] = np.sin(state['angle']) if 'angle' in state.keys() else np.sin(0.0)
 
-    s = np.zeros_like(STATE_VARIABLES, dtype=float)
+    if dtype is None:
+        dtype = np.float32
+
+    s = np.zeros_like(STATE_VARIABLES, dtype=dtype)
     for i, v in enumerate(STATE_VARIABLES):
         s[i] = state.get(v) if v in state.keys() else s[i]
     return s
@@ -60,6 +63,20 @@ def cartpole_state_varname_to_index(variable_name: str) -> int:
 
 def cartpole_state_index_to_varname(index: int) -> str:
     return STATE_VARIABLES[index]
+
+
+def cartpole_state_varnames_to_indices(variable_names: list) -> list:
+    indices = []
+    for variable_name in variable_names:
+        indices.append(cartpole_state_varname_to_index(variable_name))
+    return indices
+
+def cartpole_state_indices_to_varnames(indices: list) -> list:
+    varnames = []
+    for index in indices:
+        varnames.append(cartpole_state_index_to_varname(index))
+    return varnames
+
 
 
 def cartpole_state_namespace_to_vector(s_namespace: SimpleNamespace) -> np.ndarray:

Modeling/TF/TF_Functions/Dataset.py
@@ -213,14 +213,14 @@ class DatasetRandom(keras.utils.Sequence):
                                             high=3.0 * np.pi)
 
         initial_state = pd.DataFrame(0, index=np.arange(1),
-                                          columns=['s.angle.cos', 's.angle.sin', 's.angleD', 's.position',
-                                                   's.positionD'])
+                                          columns=['angle_cos', 'angle_sin', 'angleD', 'position',
+                                                   'positionD'])
 
-        initial_state['s.angle.cos'] = [np.cos(s[cartpole_state_varname_to_index('angle')])]
-        initial_state['s.angle.sin'] = [np.sin(s[cartpole_state_varname_to_index('angle')])]
-        initial_state['s.angleD'] = [s[cartpole_state_varname_to_index('angleD')]]
-        initial_state['s.position'] = [s[cartpole_state_varname_to_index('position')]]
-        initial_state['s.positionD'] = [s[cartpole_state_varname_to_index('positionD')]]
+        initial_state['angle_cos'] = [np.cos(s[cartpole_state_varname_to_index('angle')])]
+        initial_state['angle_sin'] = [np.sin(s[cartpole_state_varname_to_index('angle')])]
+        initial_state['angleD'] = [s[cartpole_state_varname_to_index('angleD')]]
+        initial_state['position'] = [s[cartpole_state_varname_to_index('position')]]
+        initial_state['positionD'] = [s[cartpole_state_varname_to_index('positionD')]]
 
         Predictor = predictor_ideal((self.exp_len+1) * 5, 0.02) # This results in exp_len+2 timesteps
         Predictor.setup(initial_state=initial_state, prediction_denorm=False)

Predictores/predictor_autoregressive_np.py
@@ -21,7 +21,7 @@ RNN_FULL_NAME = 'GRU-6IN-64H1-64H2-5OUT-0' # DT = 0.1s for this net
 # RNN_PATH = './save_tf/long_3_55/'
 RNN_PATH = './save_tf/'
 # RNN_PATH = './controllers/nets/mpc_on_rnn_tf/'
-PREDICTION_FEATURES_NAMES = ['s.angle.cos', 's.angle.sin', 's.angleD', 's.position', 's.positionD']
+PREDICTION_FEATURES_NAMES = ['angle_cos', 'angle_sin', 'angleD', 'position', 'positionD']
 
 class predictor_autoregressive_tf:
     def __init__(self, horizon, dt=None):
@@ -116,10 +116,10 @@ class predictor_autoregressive_tf:
 
         if self.prediction_denorm:
             predictions = copy.copy(denormalize_df(self.prediction_rnn[self.prediction_features_names], self.normalization_info))
-            predictions['s.angle'] = np.arctan2(predictions['s.angle.sin'], predictions['s.angle.cos'])
+            predictions['angle'] = np.arctan2(predictions['angle_sin'], predictions['angle_cos'])
         else:
             predictions = copy.copy(self.prediction_rnn[self.prediction_features_names])
-            predictions['s.angle'] = np.arctan2(predictions['s.angle.sin'], predictions['s.angle.cos'])/np.pi
+            predictions['angle'] = np.arctan2(predictions['angle_sin'], predictions['angle_cos'])/np.pi
 
         return predictions
 

Predictores/predictor_ideal.py
@@ -33,37 +33,43 @@ Using predictor:
 #   Updating it more often will lead to false results.
 
 
-from Modeling.load_and_normalize import load_normalization_info, normalize_df
+from Modeling.load_and_normalize import load_normalization_info, normalize_numpy_array
 from CartPole.cartpole_model import Q2u
-from CartPole._CartPole_mathematical_helpers import create_cartpole_state, cartpole_state_varname_to_index
+from CartPole._CartPole_mathematical_helpers import create_cartpole_state,\
+    cartpole_state_varname_to_index, cartpole_state_varnames_to_indices, \
+    cartpole_state_indices_to_varnames
 
 import numpy as np
-import pandas as pd
-import copy
-import timeit
 
 from CartPole.cartpole_model import cartpole_ode
 
-RNN_FULL_NAME = 'GRU-6IN-64H1-64H2-5OUT-0' # You need it to get normalization info
-RNN_PATH = './save_tf/'
-# RNN_PATH = './controllers/nets/mpc_on_rnn_tf/'
-PREDICTION_FEATURES_NAMES = ['s.angle.cos', 's.angle.sin', 's.angle', 's.angleD', 's.position', 's.positionD']
-PATH_TO_NORMALIZATION_INFO = './Modeling/NormalizationInfo/' + 'NI_2021-03-01_11-51-13.csv'
+PATH_TO_NORMALIZATION_INFO = './Modeling/NormalizationInfo/' + '2500.csv'
 
-def mpc_next_state(s, u, dt):
+def next_state(s, u, dt):
     """Wrapper for CartPole ODE. Given a current state (without second derivatives), returns a state after time dt
     """
 
-    s_next = s
+    s_next = create_cartpole_state()
 
-    s_next[cartpole_state_varname_to_index('angleDD')], s_next[cartpole_state_varname_to_index('positionDD')] = cartpole_ode(s_next, u)  # Calculates CURRENT second derivatives
+    # # Calculates CURRENT second derivatives
+    # s_next[cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = cartpole_ode(s, u)
 
     # Calculate NEXT state:
-    s_next[cartpole_state_varname_to_index('position')] = s[cartpole_state_varname_to_index('position')] + s[cartpole_state_varname_to_index('positionD')] * dt
-    s_next[cartpole_state_varname_to_index('positionD')] = s[cartpole_state_varname_to_index('positionD')] + s[cartpole_state_varname_to_index('positionDD')] * dt
+    s_next[cartpole_state_varname_to_index('position')] = \
+        s[cartpole_state_varname_to_index('position')] + s[cartpole_state_varname_to_index('positionD')] * dt
+    s_next[cartpole_state_varname_to_index('positionD')] = \
+        s[cartpole_state_varname_to_index('positionD')] + s[cartpole_state_varname_to_index('positionDD')] * dt
 
-    s_next[cartpole_state_varname_to_index('angle')] = s[cartpole_state_varname_to_index('angle')] + s[cartpole_state_varname_to_index('angleD')] * dt
-    s_next[cartpole_state_varname_to_index('angleD')] = s[cartpole_state_varname_to_index('angleD')] + s[cartpole_state_varname_to_index('angleDD')] * dt
+    s_next[cartpole_state_varname_to_index('angle')] = \
+        s[cartpole_state_varname_to_index('angle')] + s[cartpole_state_varname_to_index('angleD')] * dt
+    s_next[cartpole_state_varname_to_index('angleD')] = \
+        s[cartpole_state_varname_to_index('angleD')] + s[cartpole_state_varname_to_index('angleDD')] * dt
+
+    s_next[cartpole_state_varname_to_index('angle_cos')] = np.cos(s_next[cartpole_state_varname_to_index('angle')])
+    s_next[cartpole_state_varname_to_index('angle_sin')] = np.sin(s_next[cartpole_state_varname_to_index('angle')])
+
+    # Calculates second derivatives of NEXT state
+    s_next[cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = cartpole_ode(s_next, u)
 
     return s_next
 
@@ -83,37 +89,20 @@ class predictor_ideal:
 
         self.dt = dt
 
-        self.prediction_features_names = PREDICTION_FEATURES_NAMES
+        self.prediction_features_names = cartpole_state_indices_to_varnames(range(len(self.s)))
+
         self.prediction_denorm = False
 
-        # self.prediction_list = pd.DataFrame(columns=PREDICTION_FEATURES_NAMES, index=range(horizon + 1))
-        self.prediction_list = pd.DataFrame(data=np.zeros((self.horizon+1, len(PREDICTION_FEATURES_NAMES)+1)),
-                                            columns=['Q'] + PREDICTION_FEATURES_NAMES)
 
-        pass
+        #
+        self.output = np.zeros((self.horizon+1, len(self.prediction_features_names)+1))
 
-    def setup(self, initial_state: pd.DataFrame, prediction_denorm=False):
 
-        if ('s.angle' in initial_state.columns):
-            self.s[cartpole_state_varname_to_index('angle')] = initial_state['s.angle'].to_numpy().squeeze()
-        elif ('s.angle.cos' in initial_state.columns) and ('s.angle.sin' in initial_state.columns):
-            self.s[cartpole_state_varname_to_index('angle')] = np.arctan2(initial_state['s.angle.sin'].to_numpy(), initial_state['s.angle.cos'].to_numpy())
-        else:
-            raise ValueError('Angle info missing')
+    def setup(self, initial_state: np.ndarray, prediction_denorm=False):
 
-        if ('s.angle.cos' in initial_state.columns) and ('s.angle.sin' in initial_state.columns):
-            self.s[cartpole_state_varname_to_index('angle_cos')] = initial_state['s.angle.cos'].to_numpy().squeeze()
-            self.s[cartpole_state_varname_to_index('angle_sin')] = initial_state['s.angle.sin'].to_numpy().squeeze()
-        elif ('s.angle' in initial_state.columns):
-            self.s[cartpole_state_varname_to_index('angle_cos')] = np.cos(initial_state['s.angle']).to_numpy().squeeze()
-            self.s[cartpole_state_varname_to_index('angle_sin')] = np.sin(initial_state['s.angle']).to_numpy().squeeze()
-        else:
-            raise ValueError('Angle info missing')
+        # The initial state is provided with not valid second derivatives
 
-        self.s[cartpole_state_varname_to_index('angleD')] = initial_state['s.angleD'].to_numpy().squeeze()
-
-        self.s[cartpole_state_varname_to_index('position')] = initial_state['s.position'].to_numpy().squeeze()
-        self.s[cartpole_state_varname_to_index('positionD')] = initial_state['s.positionD'].to_numpy().squeeze()
+        self.s[:] = initial_state
 
         if prediction_denorm:
             self.prediction_denorm = True
@@ -121,48 +110,29 @@ class predictor_ideal:
             self.prediction_denorm = False
 
 
-    def predict(self, Q) -> pd.DataFrame:
+    def predict(self, Q: np.ndarray) -> np.ndarray:
 
-        if len(Q) != self.horizon:
+        if len(Q) != self.horizon+1:
             raise IndexError('Number of provided control inputs does not match the horizon')
         else:
             Q_hat = np.atleast_1d(np.asarray(Q).squeeze())
 
-        # t0 = timeit.default_timer()
-        yp_hat = np.zeros(self.horizon + 1, dtype=object)
+        self.output[:, -1] = Q_hat
+
+        s_next = self.s
+        # Calculate second derivatives of initial state
+        s_next[cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = cartpole_ode(s_next, Q2u(Q_hat[0]))
+        self.output[0, :-1] = s_next
 
         for k in range(self.horizon):
-            if k == 0:
-                yp_hat[0] = self.s
-                s_next = self.s
-
-            t0 = timeit.default_timer()
-            s_next = mpc_next_state(s_next, Q2u(Q_hat[k]), dt=self.dt)
-            s_next[cartpole_state_varname_to_index('angle_cos')] = np.cos(s_next[cartpole_state_varname_to_index('angle')])
-            s_next[cartpole_state_varname_to_index('angle_sin')] = np.sin(s_next[cartpole_state_varname_to_index('angle')])
-            t1 = timeit.default_timer()
-            # self.eq_eval_time.append((t1 - t0) * 1.0e6)
-            yp_hat[k + 1] = s_next
-
-        all_features = []
-        for k in range(len(yp_hat)):
-            s = yp_hat[k]
-            if k < self.horizon:
-                Q = Q_hat[k]
-            else:
-                Q = Q_hat[k-1]
-            timestep_features = [Q, s[cartpole_state_varname_to_index('angle_cos')], s[cartpole_state_varname_to_index('angle_sin')], s[cartpole_state_varname_to_index('angle')], s[cartpole_state_varname_to_index('angleD')], s[cartpole_state_varname_to_index('position')], s[cartpole_state_varname_to_index('positionD')]]
-            all_features.append(timestep_features)
-        all_features = np.asarray(all_features)
-        self.prediction_list.values[:, :] = all_features
-        # self.prediction_list = normalize_df(self.prediction_list, self.normalization_info)
-
-        predictions = copy.copy(self.prediction_list)
+            s_next = next_state(s_next, Q2u(Q_hat[k]), dt=self.dt)
+            self.output[k+1, :-1] = s_next
 
         if self.prediction_denorm:
-            return predictions
+            return self.output
         else:
-            return normalize_df(predictions, self.normalization_info)
+            columns = self.prediction_features_names + ['Q']
+            return normalize_numpy_array(self.output, columns, self.normalization_info)
 
     # @tf.function
     def update_internal_state(self, Q0):

Predictores/test_predictors-3.py
@@ -39,7 +39,7 @@ except:
 
 # datafile = glob.glob('./data/validate/' + '*.csv')[0]
 datafile = glob.glob('./ExperimentRecordings/Test/' + '*.csv')[0]
-features = ['s.angle.cos', 's.angle.sin', 's.angle', 's.angleD', 's.position', 's.positionD']
+features = ['angle_cos', 'angle_sin', 'angle', 'angleD', 'position', 'positionD']
 feature_to_plot = features[2]
 
 df = pd.read_csv(datafile, comment='#', dtype=np.float32)

run_data_generator.py
@@ -1,5 +1,5 @@
 from CartPole import CartPole
-from CartPole.cartpole_model import s0, TrackHalfLength
+from CartPole.cartpole_model import s0, TrackHalfLength, P_GLOBALS
 from CartPole._CartPole_mathematical_helpers import cartpole_state_varname_to_index
 
 from time import sleep
@@ -8,22 +8,25 @@ import timeit
 import numpy as np
 
 # User defined simulation settings
-csv = 'Test'  # Name with which data is saved, consecutive experiments will be save with increasing index attached
-number_of_experiments = 1  # How many experiments will be generated
-save_mode = 'offline'  # It was intended to save memory usage, but it doesn't seems to help. Leave it false.
+# csv = '2500/Test'  # Name with which data is saved, consecutive experiments will be save with increasing index attached
+# csv = '25/Train/Train'
+# csv = '25/Test/Test'
+csv = '25/Validate/Validate'
+number_of_experiments = 10  # How many experiments will be generated
+save_mode = 'online'  # It was intended to save memory usage, but it doesn't seems to help. Leave it false.
 
 # Timescales
-dt_simulation_DataGen = 0.02  # simulation timestep
-dt_controller_update_DataGen = 0.02
+dt_simulation_DataGen = 0.002  # simulation timestep
+dt_controller_update_DataGen = 0.1
 dt_save_DataGen = 0.1
 
 # CartPole settings - check the effect first in GUI before you launch big data generation
-length_of_experiment_DataGen = 6.0  # Length of each experiment in s
-controller_DataGen = 'mppi'  # Controller which should be used in generated experiment
+length_of_experiment_DataGen = 600.0  # Length of each experiment in s
+controller_DataGen = 'do-mpc'  # Controller which should be used in generated experiment
 # Possible options for controller:
 # 'manual-stabilization', 'do-mpc', 'lqr'
 track_relative_complexity_DataGen = 0.1  # randomly placed target points/s
-interpolation_type_DataGen = 'previous'  # Sets how to interpolate between turning points of random trace
+interpolation_type_DataGen = '0-derivative-smooth'  # Sets how to interpolate between turning points of random trace
 # Possible choices: '0-derivative-smooth', 'linear', 'previous'
 turning_points_period_DataGen = 'regular'  # How turning points should be distributed
 # Possible choices: 'regular', 'random'
@@ -36,33 +39,42 @@ turning_points_DataGen = None
 # initial state
 # This is just one possibility how to set the initial state. Feel free to modify this code
 # [position, positionD, angle, angleD]
-initial_state = [0, None, 20 * (np.pi / 180.0), None]
+initial_state = [None, None, None, None]
 initial_state_DataGen = s0
-if initial_state[0] is None:
-    initial_state_DataGen[cartpole_state_varname_to_index('position')] = np.random.uniform(low=-TrackHalfLength / 2.0,
-                                          high=TrackHalfLength / 2.0)
-else:
-    initial_state_DataGen[cartpole_state_varname_to_index('position')] = initial_state[0]
 
-if initial_state[1] is None:
-    initial_state_DataGen[cartpole_state_varname_to_index('positionD')] = np.random.uniform(low=-1.0,
-                                           high=1.0)
-else:
-    initial_state_DataGen[cartpole_state_varname_to_index('positionD')] = initial_state[1]
+# Set the max for smoothly interpolated random target position to avoid bumping into track ends.
+used_track_fraction = 0.5
 
-if initial_state[2] is None:
-    initial_state_DataGen[cartpole_state_varname_to_index('angle')] = np.random.uniform(low=-3.5 * (np.pi / 180.0),
-                                       high=3.5 * (np.pi / 180.0))
-else:
-    initial_state_DataGen[cartpole_state_varname_to_index('angle')] = initial_state[2]
-
-if initial_state[3] is None:
-    initial_state_DataGen[cartpole_state_varname_to_index('angleD')] = np.random.uniform(low=-3.0 * (np.pi / 180.0),
-                                        high=3.0 * (np.pi / 180.0))
-else:
-    initial_state_DataGen[cartpole_state_varname_to_index('angleD')] = initial_state[3]
 
 for i in range(number_of_experiments):
+
+    start_random_target_position_at_DataGen = 0.0
+    end_random_target_position_at_DataGen = 0.0
+
+    if initial_state[0] is None:
+        initial_state_DataGen[cartpole_state_varname_to_index('position')] = np.random.uniform(
+            low=-TrackHalfLength / 2.0,
+            high=TrackHalfLength / 2.0)
+    else:
+        initial_state_DataGen[cartpole_state_varname_to_index('position')] = initial_state[0]
+
+    if initial_state[1] is None:
+        initial_state_DataGen[cartpole_state_varname_to_index('positionD')] = np.random.uniform(low=-1.0,
+                                                                                                high=1.0) * P_GLOBALS.TrackHalfLength *0.1
+    else:
+        initial_state_DataGen[cartpole_state_varname_to_index('positionD')] = initial_state[1]
+
+    if initial_state[2] is None:
+        initial_state_DataGen[cartpole_state_varname_to_index('angle')] = np.random.uniform(low=-3.5 * (np.pi / 180.0),
+                                                                                            high=3.5 * (np.pi / 180.0))
+    else:
+        initial_state_DataGen[cartpole_state_varname_to_index('angle')] = initial_state[2]
+
+    if initial_state[3] is None:
+        initial_state_DataGen[cartpole_state_varname_to_index('angleD')] = np.random.uniform(low=-3.0 * (np.pi / 180.0),
+                                                                                             high=3.0 * (np.pi / 180.0))
+    else:
+        initial_state_DataGen[cartpole_state_varname_to_index('angleD')] = initial_state[3]
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # You may also specify some of the variables from above here, to make them change at each iteration.#
@@ -90,7 +102,8 @@ for i in range(number_of_experiments):
         turning_points_period=turning_points_period_DataGen,
         start_random_target_position_at=start_random_target_position_at_DataGen,
         end_random_target_position_at=end_random_target_position_at_DataGen,
-        turning_points=turning_points_DataGen
+        turning_points=turning_points_DataGen,
+        used_track_fraction=used_track_fraction,
     )
     gen_start = timeit.default_timer()
     CartPoleInstance.run_cartpole_random_experiment(