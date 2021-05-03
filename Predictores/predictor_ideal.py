f6ab7bb4f1d0fb680a9c6ba6edb4c1c9f60ae9c6
Frederik Heetmeyer <heerfn@yahoo.de>
2021-05-03 10:28:33 +0200

    Try predictor with cuda

--- Predictores/predictor_ideal.py
+++ Predictores/predictor_ideal.py
@@ -33,62 +33,95 @@ Using predictor:
 #   Updating it more often will lead to false results.
 
 
-from Modeling.load_and_normalize import load_normalization_info, normalize_numpy_array
-from CartPole.cartpole_model import Q2u, TrackHalfLength
-from CartPole.state_utilities import create_cartpole_state, \
-    cartpole_state_varname_to_index, cartpole_state_varnames_to_indices, \
-    cartpole_state_indices_to_varnames
+import math
 
+from numba import cuda, vectorize, float32
 import numpy as np
 
-from CartPole.cartpole_model import cartpole_ode
-
+from others.globals_and_utils import Timer
 from copy import deepcopy
 
-PATH_TO_NORMALIZATION_INFO = './Modeling/NormalizationInfo/' + '2500.csv'
+from Modeling.load_and_normalize import load_normalization_info, normalize_numpy_array
+from CartPole.cartpole_model import (
+    Q2u, cartpole_ode, _angleDD, _positionDD, get_A,
+    TrackHalfLength, k, M, m, g, J_fric, M_fric, L, v_max, u_max
+)
+param_k = np.array([k], dtype=np.float32)
+param_M = np.array([M], dtype=np.float32)
+param_m = np.array([m], dtype=np.float32)
+param_g = np.array([g], dtype=np.float32)
+param_J_fric = np.array([J_fric], dtype=np.float32)
+param_M_fric = np.array([M_fric], dtype=np.float32)
+param_L = np.array([L], dtype=np.float32)
+param_TrackHalfLength = np.array([TrackHalfLength], dtype=np.float32)
+param_dt = np.array([0.02], dtype=np.float32)
 
-def next_state(s, u, dt, intermediate_steps=2):
-    """Wrapper for CartPole ODE. Given a current state (without second derivatives), returns a state after time dt
-    """
+from CartPole.state_utilities import (
+    create_cartpole_state,
+    cartpole_state_varname_to_index, cartpole_state_varnames_to_indices,
+    cartpole_state_indices_to_varnames,
+    ANGLE_IDX, ANGLED_IDX, ANGLEDD_IDX, POSITION_IDX, POSITIOND_IDX, POSITIONDD_IDX, ANGLE_COS_IDX, ANGLE_SIN_IDX,
+    STATE_VARIABLES
+)
 
-    s_next = deepcopy(s)
+PATH_TO_NORMALIZATION_INFO = './Modeling/NormalizationInfo/' + 'NI_2021-04-22_14-34-14.csv'
+threads_per_block = 16
+blocks_per_grid = math.ceil(2000/16)
 
-    # # Calculates CURRENT second derivatives
-    # s_next[cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = cartpole_ode(s, u)
-    t_step = dt / float(intermediate_steps)
-    for i in range(intermediate_steps):
-        # Calculate NEXT state:
-        s_next[
-            ...,
-            [
-                cartpole_state_varname_to_index('position'),
-                cartpole_state_varname_to_index('positionD'),
-                cartpole_state_varname_to_index('angle'),
-                cartpole_state_varname_to_index('angleD')
-            ]
-        ] += s_next[
-            ...,
-            [
-                cartpole_state_varname_to_index('positionD'),
-                cartpole_state_varname_to_index('positionDD'),
-                cartpole_state_varname_to_index('angleD'),
-                cartpole_state_varname_to_index('angleDD')
-            ]
-        ] * t_step
-        
-        # Simulate bouncing off edges (does not consider cart dimensions)
-        s_next[..., abs(s_next[..., cartpole_state_varname_to_index('position')]) > TrackHalfLength, cartpole_state_varname_to_index('positionD')] \
-            = -s_next[..., abs(s_next[..., cartpole_state_varname_to_index('position')]) > TrackHalfLength, cartpole_state_varname_to_index('positionD')]
+# @vectorize(['float32(float32, float32)'], target='cuda')
+@cuda.jit
+def edge_bounce(position, positionD, param_TrackHalfLength):
+    start = cuda.grid(1)      # 1 = one dimensional thread grid
+    stride = cuda.gridsize(1)
+    for i in range(start, position.shape[0], stride):
+        if abs(position[i]) > param_TrackHalfLength[0]: positionD[i] = -positionD[i]
 
-        # Calculates second derivatives of NEXT state
-        angleDD, positionDD = cartpole_ode(s_next, u)
-        s_next[..., cartpole_state_varname_to_index('angleDD')] = angleDD
-        s_next[..., cartpole_state_varname_to_index('positionDD')] = positionDD
+# @vectorize(['float32(float32, float32, float32)'], target='cuda')
+@cuda.jit
+def euler_step(state, stateD, t_step):
+    start = cuda.grid(1)      # 1 = one dimensional thread grid
+    stride = cuda.gridsize(1)
+    for i in range(start, state.shape[0], stride):
+        state[i] = state[i] + stateD[i] * t_step[0]
 
-    s_next[..., cartpole_state_varname_to_index('angle_cos')] = np.cos(s_next[..., cartpole_state_varname_to_index('angle')])
-    s_next[..., cartpole_state_varname_to_index('angle_sin')] = np.sin(s_next[..., cartpole_state_varname_to_index('angle')])
+# @vectorize(['float32(float32)'], target='cuda')
+@cuda.jit
+def _cos(val, cos_val):
+    start = cuda.grid(1)      # 1 = one dimensional thread grid
+    stride = cuda.gridsize(1)
+    for i in range(start, val.shape[0], stride):
+        cos_val[i] = math.cos(val[i])
 
-    return s_next
+# @vectorize(['float32(float32)'], target='cuda')
+@cuda.jit
+def _sin(val, sin_val):
+    start = cuda.grid(1)      # 1 = one dimensional thread grid
+    stride = cuda.gridsize(1)
+    for i in range(start, val.shape[0], stride):
+        sin_val[i] = math.sin(val[i])
+
+
+@cuda.jit
+def _generate_immut(dt, k, M, m, g, J_fric, M_fric, L, TrackHalfLength):
+    i = cuda.grid(1)
+    a_in = cuda.const.array_like(param_dt)
+    dt[i] = a_in[i]
+    a_in = cuda.const.array_like(param_k)
+    k[i] = a_in[i]
+    a_in = cuda.const.array_like(param_M)
+    M[i] = a_in[i]
+    a_in = cuda.const.array_like(param_m)
+    m[i] = a_in[i]
+    a_in = cuda.const.array_like(param_g)
+    g[i] = a_in[i]
+    a_in = cuda.const.array_like(param_J_fric)
+    J_fric[i] = a_in[i]
+    a_in = cuda.const.array_like(param_M_fric)
+    M_fric[i] = a_in[i]
+    a_in = cuda.const.array_like(param_L)
+    L[i] = a_in[i]
+    a_in = cuda.const.array_like(param_TrackHalfLength)
+    TrackHalfLength[i] = a_in[i]
 
 
 
@@ -107,7 +140,18 @@ class predictor_ideal:
 
         self.horizon = horizon
 
-        self.dt = dt
+        self.intermediate_steps = 2
+        self.param_dt = np.ones_like(param_dt / float(self.intermediate_steps))
+        self.param_k = np.ones_like(param_k)
+        self.param_M = np.ones_like(param_M)
+        self.param_m = np.ones_like(param_m)
+        self.param_g = np.ones_like(param_g)
+        self.param_J_fric = np.ones_like(param_J_fric)
+        self.param_M_fric = np.ones_like(param_M_fric)
+        self.param_L = np.ones_like(param_L)
+        self.param_TrackHalfLength = np.ones_like(param_TrackHalfLength)
+
+        _generate_immut[1,1](self.param_dt, self.param_k, self.param_M, self.param_m, self.param_g, self.param_J_fric, self.param_M_fric, self.param_L, self.param_TrackHalfLength)
 
         self.prediction_features_names = cartpole_state_indices_to_varnames(range(len(self.s)))
 
@@ -119,40 +163,110 @@ class predictor_ideal:
         # The initial state is provided with not valid second derivatives
         # Batch_size > 1 allows to feed several states at once and obtain predictions parallely
 
-        # Shape of state: (batch size x state variables)
-        self.s = initial_state
-        self.batch_size = np.size(self.s, 0) if self.s.ndim > 1 else 1
+        initial_state = np.transpose(initial_state)
+        # Shape of state: (state variables x batch size)
+        if initial_state.ndim == 1: initial_state = initial_state[:, np.newaxis]
+        self.s = cuda.to_device(np.ascontiguousarray(initial_state))
+        self.batch_size = np.size(self.s, 1) if self.s.ndim > 1 else 1
+
+        self.angle, self.angleD, self.angleDD, self.position, self.positionD, self.positionDD, self.angle_cos, self.angle_sin = (
+            self.s[ANGLE_IDX, :],
+            self.s[ANGLED_IDX, :],
+            self.s[ANGLEDD_IDX, :],
+            self.s[POSITION_IDX, :],
+            self.s[POSITIOND_IDX, :],
+            self.s[POSITIONDD_IDX, :],
+            self.s[ANGLE_COS_IDX, :],
+            self.s[ANGLE_SIN_IDX, :],
+        )
+        
+        # self.angleDD_out = cuda.device_array(shape=(self.batch_size,), dtype=np.float32)
+        # self.positionDD_out = cuda.device_array(shape=(self.batch_size,), dtype=np.float32)
 
         self.prediction_denorm = prediction_denorm
 
-        self.output = np.zeros((self.batch_size, self.horizon+1, len(self.prediction_features_names)+1), dtype=np.float32)
+        self.A = cuda.device_array(shape=(self.batch_size), dtype=np.float32)
+        self.u = cuda.device_array(shape=(self.horizon, self.batch_size), dtype=np.float32)
+        self.output = np.zeros((self.horizon+1, len(self.prediction_features_names)+1, self.batch_size), dtype=np.float32)
+
+    def next_state(self, k):
+        """Wrapper for CartPole ODE. Given a current state (without second derivatives), returns a state after time dt
+        """
+        # # Calculates CURRENT second derivatives
+        # s[cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = cartpole_ode(s, u)
+        for i in range(self.intermediate_steps):
+            # Calculate NEXT state:
+            with Timer("8"):
+                euler_step[blocks_per_grid, threads_per_block](self.angle, self.angleD, self.param_dt)
+            with Timer("9"):
+                euler_step[blocks_per_grid, threads_per_block](self.angleD, self.angleDD, self.param_dt)
+            with Timer("10"):
+                euler_step[blocks_per_grid, threads_per_block](self.position, self.positionD, self.param_dt)
+            with Timer("11"):
+                euler_step[blocks_per_grid, threads_per_block](self.positionD, self.positionDD, self.param_dt)
+            
+            cuda.synchronize()
+            
+            # Simulate bouncing off edges (does not consider cart dimensions)
+            with Timer("12"):
+                edge_bounce[blocks_per_grid, threads_per_block](self.position, self.positionD, self.param_TrackHalfLength)
+            
+            cuda.synchronize()
+
+            # Calculates second derivatives of NEXT state
+            with Timer("13"):
+                get_A[blocks_per_grid, threads_per_block](self.angle_cos, self.A, self.param_k, self.param_M, self.param_m)
+            cuda.synchronize()
+
+            with Timer("14"):
+                _angleDD[blocks_per_grid, threads_per_block](self.angleD, self.angleDD, self.positionD, self.angle_cos, self.angle_sin, self.A, self.u[k, :], self.param_m, self.param_M, self.param_g, self.param_M_fric, self.param_J_fric, self.param_L)
+                _positionDD[blocks_per_grid, threads_per_block](self.angleD, self.positionD, self.positionDD, self.angle_cos, self.angle_sin, self.A, self.u[k, :], self.param_m, self.param_k, self.param_g, self.param_M_fric, self.param_J_fric, self.param_L)
+
+        with Timer("15"):
+            _cos[blocks_per_grid, threads_per_block](self.angle, self.angle_cos)
+            _sin[blocks_per_grid, threads_per_block](self.angle, self.angle_sin)
+        cuda.synchronize()
 
 
     def predict(self, Q: np.ndarray) -> np.ndarray:
+        
+        with Timer("1"):
+            # Shape of Q: (batch size x horizon length)
+            if np.size(Q, -1) != self.horizon:
+                raise IndexError('Number of provided control inputs does not match the horizon')
+            else:
+                Q_hat = np.atleast_1d(np.asarray(Q).squeeze())
+        
+        with Timer("2"):
+            # shape(u) = horizon_steps x batch_size
+            self.u = cuda.to_device(Q2u(np.ascontiguousarray(Q_hat.T)))
 
-        # Shape of Q: (batch size x horizon length)
-        if np.size(Q, -1) != self.horizon:
-            raise IndexError('Number of provided control inputs does not match the horizon')
-        else:
-            Q_hat = np.atleast_1d(np.asarray(Q).squeeze())
-
-        self.output[..., :-1, -1] = Q_hat
-
-        s_next = self.s
-        # Calculate second derivatives of initial state
-        s_next[..., cartpole_state_varnames_to_indices(['angleDD', 'positionDD'])] = np.vstack(cartpole_ode(s_next, Q2u(Q_hat[..., 0]))).T
-        self.output[..., 0, :-1] = s_next
+        with Timer("3"):
+            # Calculate second derivatives of initial state
+            get_A[blocks_per_grid, threads_per_block](self.angle_cos, self.A, self.param_k, self.param_M, self.param_m)
+        cuda.synchronize()
+        with Timer("4"):
+            _angleDD[blocks_per_grid, threads_per_block](self.angleD, self.angleDD, self.positionD, self.angle_cos, self.angle_sin, self.A, self.u[0, :], self.param_m, self.param_M, self.param_g, self.param_M_fric, self.param_J_fric, self.param_L)
+            _positionDD[blocks_per_grid, threads_per_block](self.angleD, self.positionD, self.positionDD, self.angle_cos, self.angle_sin, self.A, self.u[0, :], self.param_m, self.param_k, self.param_g, self.param_M_fric, self.param_J_fric, self.param_L)
+        with Timer("5"):
+            self.output[0, :-1, :] = np.stack([self.angle, self.angleD, self.angleDD, self.position, self.positionD, self.positionDD, self.angle_cos, self.angle_sin])
 
         for k in range(self.horizon):
-            s_next = next_state(s_next, Q2u(Q_hat[..., k]), dt=self.dt, intermediate_steps=2)
-            self.output[..., k+1, :-1] = s_next
+            with Timer("6"):
+            # Inplace update of state
+                self.next_state(k)
+            with Timer("6.2"):
+                self.output[k+1, :-1, :] = np.stack([self.angle, self.angleD, self.angleDD, self.position, self.positionD, self.positionDD, self.angle_cos, self.angle_sin])
 
-        self.output = np.squeeze(self.output)
-        if self.prediction_denorm:
-            return self.output[..., :-1]
-        else:
-            columns = self.prediction_features_names + ['Q']
-            return normalize_numpy_array(self.output, columns, self.normalization_info)[..., :-1]
+        # self.output = self.output.copy_to_host()
+        with Timer("7"):
+            self.output = np.squeeze(self.output)
+            if self.prediction_denorm:
+                return np.transpose(self.output[:, :-1, :], axes=(2,0,1))
+            else:
+                self.output[:-1, -1, :] = np.transpose(Q_hat)
+                columns = self.prediction_features_names + ['Q']
+                return normalize_numpy_array(np.transpose(self.output, axes=(2,0,1)), columns, np.squeeze(self.normalization_info)[:, :-1])
 
     # @tf.function
     def update_internal_state(self, Q0):
