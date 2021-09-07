9b30178b4d5ef68edfc8c1f0f9a10ae686da3160
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2021-09-07 18:21:24 +0200

    Implemented additional options for discretisation and estimating state through IIR filter as we do till now in physical cartpole software. Luenberger observer not working yet

--- Controllers/controller_lqr_observer.py
+++ Controllers/controller_lqr_observer.py
@@ -20,6 +20,23 @@ config = yaml.load(open("config.yml", "r"), Loader=yaml.FullLoader)
 Q = np.diag(config["controller"]["lqr"]["Q"])
 R = config["controller"]["lqr"]["R"]
 
+Ts = 0.02  # sampling frequency in sec
+
+# MODE = 'Continuous'
+MODE = 'Discrete'
+
+# DISCRETISATION = 'Euler'
+DISCRETISATION = 'Exact'
+
+OBSERVER = 'Luenberger'
+# OBSERVER = 'Euler + IIR Filter'
+
+# Variables for 'Euler + IIR Filter'
+angle_smoothing = 0.6
+position_smoothing = 0.6
+angleD_smoothing = 0.8
+positionD_smoothing = 0.8
+
 class controller_lqr_observer(template_controller):
     def __init__(self):
         # From https://github.com/markwmuller/controlpy/blob/master/controlpy/synthesis.py#L8
@@ -33,12 +50,20 @@ class controller_lqr_observer(template_controller):
 
         where Q is a positive semidefinite matrix, and R is positive definite matrix.
 
-        Returns K, X, eigVals:
-        Returns gain the optimal gain K, the solution matrix X, and the closed loop system eigenvalues.
+        Returns K, P, eigVals:
+        Returns gain the optimal gain K, the solution matrix P, and the closed loop system eigenvalues.
         The optimal input is then computed as:
          input: u = -K*x
         """
         # ref Bertsekas, p.151
+
+        print('--------------------------------------------------')
+        print('LQR')
+        print('MODE: {}'.format(MODE))
+        if MODE == 'Discrete':
+            print('DISCRETIZATION: {}'.format(DISCRETISATION))
+        print('OBSERVER: {}'.format(OBSERVER))
+        print('--------------------------------------------------')
 
         self.controller_data_for_csv = {
                                         'angle_estimate': [0.0],
@@ -65,45 +90,57 @@ class controller_lqr_observer(template_controller):
         jacobian = cartpole_jacobian(s, u)
         self.A = jacobian[:, :-1]
         self.B = np.reshape(jacobian[:, -1], newshape=(4, 1)) * u_max
-        self.C = np.array([[1, 0, 0, 0],
-                            [0, 0, 1, 0]])
+        self.C = np.array([[1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0]])
+        self.D = np.array([[0.0],
+                           [0.0]])
+        if DISCRETISATION == 'Euler':
+            self.Ad = np.eye(4) + Ts * self.A
+            self.Bd = Ts * self.B
+            self.Cd = self.C
+            self.Dd = self.D
+        elif DISCRETISATION == 'Exact':
+            self.Ad, self.Bd, self.Cd, self.Dd, _ = scipy.signal.cont2discrete((self.A, self.B, self.C, self.D), dt=Ts, method='zoh')
 
         # Cost matrices for LQR controller
         self.Q = Q  # How much to punish x, v, theta, omega
         self.R = R  # How much to punish Q
 
-        # first, try to solve the ricatti equation
-        X = scipy.linalg.solve_continuous_are(self.A, self.B, self.Q, self.R)
 
+        # Solve the ricatti equation
+        P = scipy.linalg.solve_continuous_are(self.A, self.B, self.Q, self.R)
         # compute the LQR gain
         if np.array(self.R).ndim == 0:
             Ri = 1.0 / self.R
         else:
             Ri = np.linalg.inv(self.R)
 
-        self.K = np.dot(Ri, (np.dot(self.B.T, X)))
+        self.K = np.dot(Ri, (np.dot(self.B.T, P)))
+
+        Pd = scipy.linalg.solve_discrete_are(self.Ad, self.Bd, self.Q, self.R)
+        self.Kd = np.linalg.inv(self.R+self.Bd.T@Pd@self.Bd)@self.Bd.T@Pd@self.Ad
 
         poles_system = np.linalg.eigvals(self.A - np.dot(self.B, self.K))
-        slowest_pole_ct = poles_system[np.argmax(poles_system)]
+        poles_system_d = np.linalg.eigvals(self.Ad-self.Bd@self.Kd)
+
+        # It is not necessary to solve are separately for discrete case
+        # K=Kd and poles_system_d = exp(Ts*poles_system)
+
+        slowest_pole_ct = poles_system[np.argmax(poles_system.real)]
         pole1_ct = slowest_pole_ct * 10
         pole2_ct = np.conj(pole1_ct)
         poles_obs_ct = np.array([pole1_ct, pole1_ct-1, pole2_ct, pole2_ct-1])
         print('poles obs ct', poles_obs_ct)
 
-        # Discretize continuous time system
-        Ts = 0.02 # sampling frequency in sec
-        # print(np.exp(Ts*poles_system))
-        poles_obs = np.exp(Ts*poles_obs_ct)
-        print('poles obs dt',poles_obs)
-        Ad = np.eye(4) + Ts * self.A
-        Bd = Ts * self.B
-        Cd = self.C
-        # poles_system = np.linalg.eigvals(Ad - np.dot(Bd, self.K))
-        # print(poles_system)
-
         # Luenberger observer gain
-        obs = signal.place_poles(Ad.T, Cd.T, poles_obs.T)
-        self.Kf = obs.gain_matrix.T
+        # obs = signal.place_poles(Ad.T, Cd.T, poles_obs.T)
+        # self.Kf = obs.gain_matrix.T
+
+        # Marcin's implementation
+        # poles_system = np.linalg.eigvals(self.A - np.dot(self.B, self.K))
+        # poles_obs_ct = poles_system*10.0
+        # obs = signal.place_poles(self.A.T, self.C.T, poles_obs_ct)
+        # self.Kf = obs.gain_matrix.T
 
 
         self.state_estimate = np.array(
@@ -116,6 +153,7 @@ class controller_lqr_observer(template_controller):
         )
 
         self.next_state_estimate = np.copy(self.state_estimate)
+        self.state_derivative = np.copy(self.state_estimate)
 
         # For filter as it is now in physical cartpole
         self.previous_state_estimate = np.copy(self.state_estimate)
@@ -135,49 +173,55 @@ class controller_lqr_observer(template_controller):
         # Output_measurement is what you receive at every step
         output_measurement = np.array(
             [
-                [(s[cartpole_state_varname_to_index('position')])*(1+0.05*self.rng_lqr_observer.standard_normal())],
-                [s[cartpole_state_varname_to_index('angle')]*(1+0.05*self.rng_lqr_observer.standard_normal())],
+                [(s[cartpole_state_varname_to_index('position')])*(1+0.00*self.rng_lqr_observer.standard_normal())],
+                [s[cartpole_state_varname_to_index('angle')]*(1+0.00*self.rng_lqr_observer.standard_normal())],
             ]
         )
 
+        if OBSERVER == 'Euler + IIR Filter':
+            # Estimation of the state by euler integration and filtering
 
-        # Estimation of the state by euler integration and filtering
-        deltaTime = 0.02
-        angle_smoothing = 0.6
-        position_smoothing = 0.6
-        angleD_smoothing = 0.8
-        positionD_smoothing = 0.8
+            self.state_estimate[2, 0] = output_measurement[1, 0] * (angle_smoothing) + (1 - angle_smoothing) * self.previous_state_estimate[2, 0]  # filter angle
+            self.state_estimate[0, 0] = output_measurement[0, 0] * (position_smoothing) + (1 - position_smoothing) * self.previous_state_estimate[0, 0]  #filter position
 
-        self.state_estimate[2, 0] = output_measurement[1, 0] * (angle_smoothing) + (1 - angle_smoothing) * self.previous_state_estimate[2, 0]  # filter angle
-        self.state_estimate[0, 0] = output_measurement[0, 0] * (position_smoothing) + (1 - position_smoothing) * self.previous_state_estimate[0, 0]  #filter position
+            self.state_estimate[3, 0] = (self.state_estimate[2, 0] - self.previous_state_estimate[2, 0]) / Ts  # angleD
+            self.state_estimate[1, 0] = (self.state_estimate[0, 0] - self.previous_state_estimate[0, 0]) / Ts  # positionD
 
-        self.state_estimate[3, 0] = (self.state_estimate[2, 0] - self.previous_state_estimate[2, 0]) / deltaTime  # angleD
-        self.state_estimate[1, 0] = (self.state_estimate[0, 0] - self.previous_state_estimate[0, 0]) / deltaTime  # positionD
+            self.state_estimate[3] = self.state_estimate[3] * (angleD_smoothing) + (1 - angleD_smoothing) * self.previous_state_estimate[3]  # Filter angleD
+            self.state_estimate[1] = self.state_estimate[1] * (positionD_smoothing) + (1 - positionD_smoothing) * self.previous_state_estimate[1]  # Filter positionD
 
-        self.state_estimate[3] = self.state_estimate[3] * (angleD_smoothing) + (1 - angleD_smoothing) * self.previous_state_estimate[3]  # Filter angleD
-        self.state_estimate[1] = self.state_estimate[1] * (positionD_smoothing) + (1 - positionD_smoothing) * self.previous_state_estimate[1]  # Filter positionD
+            self.previous_state_estimate[...] = self.state_estimate[...]
 
-        self.previous_state_estimate[...] = self.state_estimate[...]
-
-        # # Here comes your implementation of observer
-        # self.state_estimate = self.next_state_estimate
-        # output_estimate = self.state_estimate[(0, 2), :]  # y^ = C*x^
-        #
-        # # TODO: IMPLEMENT THE LUENBERGER OBSERVER
-        # # Here comes your implementation of observer - dont' trust anything here
-        # self.state_estimate = self.next_state_estimate
-        # output_estimate = self.state_estimate[(0, 2), :]  # y^ = C*x^
-        # # self.next_state_estimate = np.dot((self.A - self.B * self.K), self.state_estimate) + np.dot(self.Kf, (
-        # #             output_measurement - output_estimate))
+            state_estimate_centered = np.copy(self.state_estimate)
+            state_estimate_centered[0] -= target_position
 
 
+        elif OBSERVER == 'Luenberger':
+            # self.state_estimate = state_true
+            self.state_estimate = self.next_state_estimate
+            state_true_centered = np.copy(state_true)
+            state_true_centered[0] -= target_position
+            state_estimate_centered = np.copy(self.state_estimate)
+            state_estimate_centered[0] -= target_position
+            output_estimate = self.state_estimate[(0, 2), :]  # y = C*x
 
-        # Q should be calculated based on just the state_estimate
-        # You can use state_true for reference solution
-        # Q = np.asscalar(np.dot(-self.K, state_true))
-        state_estimate_centered = np.copy(self.state_estimate)
-        state_estimate_centered[3] -= target_position
-        Q = np.asscalar(np.dot(-self.K, state_estimate_centered))
+            if MODE == 'Continuous':
+                self.state_estimate = self.next_state_estimate
+                self.state_derivative = (self.A - self.B @ self.K) @ state_estimate_centered  # observer: + self.Kf@(output_measurement - output_estimate)  right?
+                self.next_state_estimate = self.state_estimate + Ts*self.state_derivative
+
+            elif MODE == 'Discrete':
+                self.state_estimate = self.next_state_estimate
+                self.next_state_estimate = (self.Ad-self.Bd@self.Kd)@state_estimate_centered  # + observer, not implemented
+                self.next_state_estimate[0] += target_position
+
+
+
+
+        # Q should be calculated based on just the state_estimate(centered)
+        # You can use state_true(centered) for reference solution
+        Q = np.asscalar(np.dot(-self.K, state_true_centered))
+        # Q = np.asscalar(np.dot(-self.K, state_estimate_centered))
 
         # Clip Q
         if Q > 1.0:
