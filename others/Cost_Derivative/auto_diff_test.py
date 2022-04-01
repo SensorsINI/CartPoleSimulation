6748c7376ccd2be7a66803225465c74c94762823
diegobo <diegobo@student.ethz.ch>
2022-04-01 07:58:48 +0200

    Working autodiff for batches

--- others/Cost_Derivative/auto_diff_test.py
+++ others/Cost_Derivative/auto_diff_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import SFC64, Generator
 
 from Controllers.template_controller import template_controller
 from CartPole.cartpole_model import TrackHalfLength
@@ -13,6 +14,7 @@ from SI_Toolkit.Predictors.predictor_ODE import predictor_ODE
 from SI_Toolkit.Predictors.predictor_ODE_tf_pure import predictor_ODE_tf_pure
 from SI_Toolkit.Predictors.predictor_autoregressive_tf import predictor_autoregressive_tf
 import tensorflow as tf
+import tensorflow_probability as tfp
 tf.config.run_functions_eagerly(False)
 
 @tf.function(jit_compile = True)
@@ -21,8 +23,8 @@ def grad_desc(u,s):
             tape.watch(u)
             rollout_trajectory = predictor.predict_tf(s,u)
             cost = rollout_trajectory[:,-1,POSITION_IDX]**2
-        dc_du = tape.gradient(cost,u)
-        # dc_du = 0.0
+        # dc_du = tape.gradient(cost,u)
+        dc_du = 0.0
         return dc_du,cost
 
 # @tf.function(jit_compile = True)
@@ -52,25 +54,35 @@ s[ANGLED_IDX] = 0.237
 
 s_org = tf.convert_to_tensor(s)
 
-u_org = tf.Variable([0.594623, 0.11093523, -0.32577565, 0.36339644, 0.19863953,
-                 -0.67005044, -0.00572653, 0.50473666, 0.82851535, 0.03227299,
-                 -0.89665616, -1., -0.15769833, -0.8742089, -0.00434032,
-                 -0.5908449, -0.8486508, 0.46566853, -0.26742178, -0.2585441,
-                 -1., 1., -1., 0.820513, 1.,
-                 0.65235853, 0.7771242, -0.834638, 0.9568739, 0.21720093,
-                 -0.18284637, 0.9694907, 0.68292177, -1., 1.,
-                 0.37337917, -0.46058115, -0.6156913, 0.52652395, 0.06510112,
-                 -0.13692386, 0.4193466, 0.08954383, -0.02065406, 0.7458399,
-                 -1., 0.83411133, -0.5809542, -0.5786972, -0.70775455],
-                dtype=tf.float32)
+# u_org = tf.Variable([0.594623, 0.11093523, -0.32577565, 0.36339644, 0.19863953,
+#                  -0.67005044, -0.00572653, 0.50473666, 0.82851535, 0.03227299,
+#                  -0.89665616, -1., -0.15769833, -0.8742089, -0.00434032,
+#                  -0.5908449, -0.8486508, 0.46566853, -0.26742178, -0.2585441,
+#                  -1., 1., -1., 0.820513, 1.,
+#                  0.65235853, 0.7771242, -0.834638, 0.9568739, 0.21720093,
+#                  -0.18284637, 0.9694907, 0.68292177, -1., 1.,
+#                  0.37337917, -0.46058115, -0.6156913, 0.52652395, 0.06510112,
+#                  -0.13692386, 0.4193466, 0.08954383, -0.02065406, 0.7458399,
+#                  -1., 0.83411133, -0.5809542, -0.5786972, -0.70775455],
+#                 dtype=tf.float32)
+SEED = 5876
+rng_gen = Generator(SFC64(SEED))
+dist_var = 0.5*np.ones([1,50])
+stdev = np.sqrt(dist_var)
+num_rollouts = 10000
+dist_mue = np.zeros([1,50])
+Q = np.tile(dist_mue,(num_rollouts,1))+ np.multiply(rng_gen.standard_normal(
+                size=(num_rollouts, 50), dtype=np.float32),stdev)
+Q = np.clip(Q, -1.0, 1.0, dtype=np.float32)
 
-u_alt = tf.reverse(u_org,axis=[0])
-u = u_org
+
 s = s_org
-s = tf.stack([s,s],axis=0)
-rollout_trajectory = predictor.predict_tf(s, u[tf.newaxis, :, tf.newaxis])
+s = np.tile(s,tf.constant([num_rollouts,1]))
+u = tf.convert_to_tensor(Q)
+u = u[:, :, tf.newaxis]
+rollout_trajectory = predictor.predict_tf(s, u)
 lr = 10
-u = tf.stack([u[ :, tf.newaxis],u_alt[ :, tf.newaxis]],axis = 0)
+
 test(u,s)
 
 import timeit
