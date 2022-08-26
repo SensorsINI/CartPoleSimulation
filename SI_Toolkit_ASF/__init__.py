2d575d340c6de754a98d4964de69b1c391db2461
heetmeyer <heerfn@yahoo.de>
2022-08-26 18:46:24 +0200

    Add option to disable jit compilation

--- SI_Toolkit_ASF/__init__.py
+++ SI_Toolkit_ASF/__init__.py
@@ -1,1 +1,2 @@
-GLOBALLY_DISABLE_COMPILATION = False
+GLOBALLY_DISABLE_COMPILATION = False  # Set to False to use tf.function
+USE_JIT_COMPILATION = False  # XLA ignores random seeds. Set to False for reproducibility
