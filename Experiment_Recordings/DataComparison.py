3e2bbe69a2c0bbfb8bcfab77a8ce381c1ae8b82a
Paul Buttler <pbuttler@student.ethz.ch>
2022-09-20 12:42:04 +0200

    Changes in DataComparison

--- Experiment_Recordings/DataComparison.py
+++ Experiment_Recordings/DataComparison.py
@@ -5,9 +5,9 @@ from scipy.fft import rfft, rfftfreq
 """Generates and saves Plots of the swing-up task from data files as well as generates plots of discrete fourier
  transforms."""
 
-
-foldernames = ['./Ground Truth', './Dense-48', './GRU-16', './Dense-96', './GRU-32-0', './GRU-32-1', './Dense-96-L',
-               './GRU-32-L-0', './GRU-32-L-1', './GRU-32-L-3', './GRU-32-L-5']
+foldernames = ['./Ground Truth', './Dense-48', './GRU-16', './Dense-96', './GRU-32-0', './GRU-32-1', './GRU-32-2',
+               './Dense-96-L', './GRU-32-L-0', './GRU-32-L-1', './GRU-32-L-3', './GRU-32-L-5', './GRU-32-new',
+               './GRU-32-new-2', './GRU-32-new-2 single', './Dense new-2', './Dense new-2 single']
 
 
 def swing_up_task(file_start=0, file_end=4, folder_number=0, pole_length='L395', fig_size=(6, 8), save=False,
@@ -27,7 +27,7 @@ def swing_up_task(file_start=0, file_end=4, folder_number=0, pole_length='L395',
     fig, axs = plt.subplots(len(experiments), sharex=True, sharey=True, figsize=fig_size, dpi=200)
     for j, file in enumerate(experiments):
         filename = foldernames[folder_number] + file
-        data = np.genfromtxt(filename, skip_header=28, delimiter=',')
+        data = np.genfromtxt(filename, skip_header=28, delimiter=',')[500:]
         time = data[:, 0]
         angle = data[:, 1] * 180 / np.pi
         axs[j].plot(time, angle, marker='.', linestyle='', markersize=2)
@@ -37,12 +37,12 @@ def swing_up_task(file_start=0, file_end=4, folder_number=0, pole_length='L395',
         # Determine Swing-Up time
         sw_up = False
         sw_try = False
-        for k in range(len(angle) - 100):
+        for k in range(len(angle) - 75):
             if np.abs(angle[k]) < 10:
                 sw_try = True
                 sw_up = True
-                for n in range(100):
-                    if np.abs(angle[k + n]) >= 10:  # pole must stay upright for 2s
+                for n in range(75):
+                    if np.abs(angle[k + n]) >= 10:  # pole must stay upright for 1.5s
                         sw_up = False
 
                 if sw_up:
@@ -52,21 +52,28 @@ def swing_up_task(file_start=0, file_end=4, folder_number=0, pole_length='L395',
                     axs[j].legend(loc='upper right')
                     break
         if sw_try and sw_up == False:
-            sw_up_time += 10  # if try was made count 10s for swing up
-            axs[j].vlines(10, ymin=-180, ymax=180, label='Attempt: 10s',
+            sw_up_time += 10  # if try was made count 10/20 s for swing up
+            axs[j].vlines(10, ymin=0, ymax=1, label='Attempt',
                           color='tab:orange')
             axs[j].legend(loc='upper right')
 
-    sw_up_time /= len(experiments)
+        if not sw_try:
+            sw_up_time += 30
+    if sw_up_time > 80:
+        sw_up_time = 0.0
+    else:
+        sw_up_time /= len(experiments)
 
     # add title and save figure
     axs[len(experiments) - 1].set(xlabel='time /s')
-    fig.suptitle(foldernames[folder_number].replace('./', '') + '\n Angle Plot Swing-Up ' + pole_length +
-                 '\n \n Average Swing-Up time: ' + str(np.around(sw_up_time, 1)) + 's')
+    fig.suptitle('Angle Plot Swing-Up ' + pole_length + '\n') \
+        # + '\n \n Average Swing-Up time: ' + str(np.around(sw_up_time, 1))+ 's')
     plt.tight_layout()
+
     if save:
-        plt.savefig(foldernames[folder_number] + '/Angle Plot Swing-Up ' + pole_length + save_name + '.pdf')
-        plt.savefig(foldernames[folder_number] + '/Angle Plot Swing-Up ' + pole_length + save_name + '.png')
+        # plt.savefig(foldernames[folder_number] + '/' + foldernames[folder_number].replace('./', '') + ' Angle Plot Swing-Up ' + pole_length + save_name + '.pdf')
+        plt.savefig(
+            foldernames[folder_number].replace('./', '') + ' Angle Plot Swing-Up ' + pole_length + save_name + '.png')
     plt.show()
 
     return sw_up_time
@@ -103,7 +110,7 @@ def fourier_transforms(file, fig_size=(6, 4), save=False):
     axs[1].grid(True)
     axs[1].legend()
 
-    fig.suptitle(file)
+    # fig.suptitle(file)
     fig.tight_layout()
     plt.show()
     if save:
@@ -112,11 +119,15 @@ def fourier_transforms(file, fig_size=(6, 4), save=False):
 
 
 def main():
-    x = [0, 4, 5]
+    x = [7, 8]
     for i in x:
-        swing_up_task(file_start=0, file_end=9, folder_number=i, pole_length='L395', fig_size=(6, 9), save=True,
-                      save_name='-1')
+        swing_up_task(file_start=1, file_end=3, folder_number=i, pole_length='L395', fig_size=(6, 7), save=True,
+                      save_name='')
 
 
 if __name__ == '__main__':
-    fourier_transforms('Repeated swing-up-2 L790', fig_size=(6, 4.5), save=True)
+    # fourier_transforms('Repeated swing-up-2 L790', fig_size=(6, 4.5), save=False)
+    swing_up_task(file_start=0, file_end=2, folder_number=16, pole_length='L395', fig_size=(6, 7), save=True,
+                  save_name='')
+
+    # main()
