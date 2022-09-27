8109f96dc884d574394b02ce29de826e11fc03ae
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-09-27 04:57:03 +0200

    Changes required by new versions of Control_Toolkit and SI_Toolkit

--- SI_Toolkit_ASF/predictors_customization_tf.py
+++ SI_Toolkit_ASF/predictors_customization_tf.py
@@ -17,7 +17,7 @@ STATE_INDICES_TF = tf.lookup.StaticHashTable(
 
 class next_state_predictor_ODE_tf():
 
-    def __init__(self, dt, intermediate_steps, batch_size=1, disable_individual_compilation=False):
+    def __init__(self, dt, intermediate_steps, batch_size=1, disable_individual_compilation=False, planning_environment=None):
         self.s = tf.convert_to_tensor(create_cartpole_state())
 
         self.intermediate_steps = tf.convert_to_tensor(intermediate_steps, dtype=tf.int32)
