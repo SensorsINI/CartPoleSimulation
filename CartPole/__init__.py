0e7547069203bd8410e87c74789699e714ae4cee
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-11-09 01:11:06 +0100

    Importing Compile Adaptive

--- CartPole/__init__.py
+++ CartPole/__init__.py
@@ -869,7 +869,7 @@ class CartPole(EnvironmentBatched):
             if self.controller.has_optimizer:
                 self.controller.configure(self.optimizer_name)
                 self.optimizer_name, self.optimizer_idx = get_optimizer_name(
-                    optimizer_name=self.controller.optimizer_name
+                    optimizer_name=self.controller.optimizer.optimizer_name
                 )
 
             else:
