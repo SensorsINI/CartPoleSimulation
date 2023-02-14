ead9c7984a91b7fcc57230258bce5dbe8ed52dda
tobidelbruck <todelbru@ethz.ch>
2023-02-14 11:06:14 +0100

    updated config_cost_functions.yml to match physical-cartpole better, not really working so well however for spin updated p_globals.py to include natural period of oscillation of rod

--- others/p_globals.py
+++ others/p_globals.py
@@ -8,6 +8,15 @@ CARTPOLE_PHYSICAL_CONSTANTS=config.cartpole # take this branch as the constants 
 
 CARTPOLE_PHYSICAL_CONSTANTS.TrackHalfLength = (CARTPOLE_PHYSICAL_CONSTANTS.usable_track_length - CARTPOLE_PHYSICAL_CONSTANTS.cart_length) / 2.0  # m, effective length, by which cart center can move
 
+# https://en.wikipedia.org/wiki/Pendulum_(mechanics), https://en.wikipedia.org/wiki/List_of_moments_of_inertia
+# T=2*pi*sqrt(I/(mgL)) where I=1/3mL^2,  where T is period, L is length of rod, m is rod mass, I is moment of inertia at end of rod
+pole_length=CARTPOLE_PHYSICAL_CONSTANTS.L*2
+pole_mass=CARTPOLE_PHYSICAL_CONSTANTS.m_pole
+Iend=(1./3.)*pole_mass*pole_length**2
+g=9.8
+natural_period=2*np.pi*np.sqrt(Iend/(pole_mass*g*pole_length))
+CARTPOLE_PHYSICAL_CONSTANTS.NaturalPeriod=natural_period
+
 # Export variables as global
 def export_globals():
     return (
