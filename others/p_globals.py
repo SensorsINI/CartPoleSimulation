corrected formula for natural frequency based on error pointed out by Pao. Thanks Pao!

others/p_globals.py
@@ -10,12 +10,18 @@ CARTPOLE_PHYSICAL_CONSTANTS=config.cartpole # take this branch as the constants 
 CARTPOLE_PHYSICAL_CONSTANTS.TrackHalfLength = (CARTPOLE_PHYSICAL_CONSTANTS.usable_track_length - CARTPOLE_PHYSICAL_CONSTANTS.cart_length) / 2.0  # m, effective length, by which cart center can move
 
 # https://en.wikipedia.org/wiki/Pendulum_(mechanics), https://en.wikipedia.org/wiki/List_of_moments_of_inertia
-# T=2*pi*sqrt(I/(mgL)) where I=1/3mL^2,  where T is period, L is length of rod, m is rod mass, I is moment of inertia at end of rod
+# T=2*pi*sqrt(I/(mg(L/2)))  where
+# T is period,
+# L is length of rod,
+# I=1/3mL^2 is moment of inertia at end of rod
+# L/2 is distance from pivot axle to center of mass of rod (i.e. half of rod length),
+# m is rod mass
+# g is gravitational acceleration
 pole_length=CARTPOLE_PHYSICAL_CONSTANTS.L*2
 pole_mass=CARTPOLE_PHYSICAL_CONSTANTS.m_pole
 Iend=(1./3.)*pole_mass*pole_length**2
 g=9.8
-natural_period=2*np.pi*np.sqrt(Iend/(pole_mass*g*pole_length))
+natural_period=2*np.pi*np.sqrt(Iend/(pole_mass*g*(pole_length/2)))
 CARTPOLE_PHYSICAL_CONSTANTS.NaturalPeriod=natural_period
 log.info(f'computed natural period of cartpole T={CARTPOLE_PHYSICAL_CONSTANTS.NaturalPeriod:.3f}s, natural frequency={1/CARTPOLE_PHYSICAL_CONSTANTS.NaturalPeriod:.3f}Hz')
 # tobi measured natural frequency of 1.08Hz, computed is 1.37Hz