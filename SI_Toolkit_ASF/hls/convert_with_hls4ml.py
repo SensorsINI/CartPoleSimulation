b80cadba0e5d465824c70b4e31d7826159859b6e
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-03-04 14:09:53 +0100

    First High Level Synthesis Script synthesising our dense neural networks to FPGA IP block with hls4ml. Vitis HLS is not yet supported. Tested with Vivado HLS version 2020.1.

---
+++ SI_Toolkit_ASF/hls/convert_with_hls4ml.py
@@ -0,0 +1,43 @@
+from types import SimpleNamespace
+
+from SI_Toolkit.Functions.General.Initialization import get_net
+
+import os
+os.environ['PATH'] = '/tools/Xilinx/Vivado/2020.1/bin:' + os.environ['PATH']
+
+# Parameters:
+PATH_TO_MODELS = './SI_Toolkit_ASF/Experiments/NeuralImitatorCPS_v2/Models/'
+NET_NAME = 'Dense-6IN-128H1-128H2-1OUT-1'
+
+# Import network
+
+a = SimpleNamespace()
+batch_size = 1
+
+a.path_to_models = PATH_TO_MODELS
+a.net_name = NET_NAME
+
+# Create a copy of the network suitable for inference (stateful and with sequence length one)
+model, net_info = \
+    get_net(a, time_series_length=1,
+            batch_size=batch_size, stateful=True)
+
+import hls4ml
+config = hls4ml.utils.config_from_keras_model(model, granularity='model')
+print("-----------------------------------")
+hls_model = hls4ml.converters.convert_from_keras_model(model,
+                                                       hls_config=config,
+                                                       output_dir='../../m1/prj0',
+                                                       board='zybo-z7-20')  # TODO: What would be counterpart for Zybo
+
+hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file=None)
+
+# TODO: Here could come a testing part...
+
+
+# Synthesis
+hls_model.build(csim=False)
+
+
+# Reports
+hls4ml.report.read_vivado_report('../m1/prj0/')
