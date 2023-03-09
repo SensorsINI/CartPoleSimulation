4d9024f6631442a633dfb82df99d5ebf028d394b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-03-09 15:02:02 +0100

    HLS now with dedicated config_hls

--- SI_Toolkit_ASF/hls/convert_with_hls4ml.py
+++ SI_Toolkit_ASF/hls/convert_with_hls4ml.py
@@ -1,38 +1,37 @@
+import yaml
+import os
+import hls4ml
+
 from types import SimpleNamespace
 
 from SI_Toolkit.Functions.General.Initialization import get_net
 
-import os
-os.environ['PATH'] = '/tools/Xilinx/Vivado/2020.1/bin:' + os.environ['PATH']
+
+
+config_hls = yaml.load(open(os.path.join('SI_Toolkit_ASF', 'config_hls.yml'), 'r'), Loader=yaml.FullLoader)
+
+os.environ['PATH'] = config_hls['path_to_hls_installation'] + ":" + os.environ['PATH']
 
 # Parameters:
-PATH_TO_MODELS = './SI_Toolkit_ASF/Experiments/NeuralImitatorCPS_v2/Models/'
-NET_NAME = 'Dense-6IN-128H1-128H2-1OUT-1'
+a = SimpleNamespace()
+batch_size = config_hls['batch_size']
+a.path_to_models = config_hls['path_to_models']
+a.net_name = config_hls['net_name']
 
 # Import network
-
-a = SimpleNamespace()
-batch_size = 1
-
-a.path_to_models = PATH_TO_MODELS
-a.net_name = NET_NAME
-
 # Create a copy of the network suitable for inference (stateful and with sequence length one)
 model, net_info = \
     get_net(a, time_series_length=1,
             batch_size=batch_size, stateful=True)
 
-import hls4ml
 config = hls4ml.utils.config_from_keras_model(model, granularity='model')
 print("-----------------------------------")
 hls_model = hls4ml.converters.convert_from_keras_model(model,
                                                        hls_config=config,
-                                                       output_dir='../../m1/prj0', ## !!!! If the path is longer it crashes. Depending on how long is the path it crashes at different places.
-                                                       board='zybo-z7-20')  # TODO: What would be counterpart for Zybo
+                                                       output_dir=config_hls['output_dir'], ## !!!! If the path is long it crashes. Depending on how long is the path it crashes at different places.
+                                                       board=config_hls['board'])  # TODO: What would be counterpart for Zybo
 
 hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file=None)
-
-# TODO: Here could come a testing part...
 
 
 # Synthesis
@@ -40,4 +39,5 @@ hls_model.build(csim=False)
 
 
 # Reports
-hls4ml.report.read_vivado_report('../m1/prj0/')
+hls4ml.report.read_vivado_report(config_hls['output_dir'])
+
