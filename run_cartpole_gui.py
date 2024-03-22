b26b7e7fe12033b549f71d423e7d5db36e9b805b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-03-22 04:11:03 +0100

    What I don't want to pull to master

--- run_cartpole_gui.py
+++ run_cartpole_gui.py
@@ -2,11 +2,6 @@
 This script starts CartPole GUI from main folder
 """
 
-# import os
-# print('GPU Disabled')
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # TF: If uncommented, only uses CPU
-# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
-
 from GUI import run_gui
 
 run_gui()
