f8b59bf909592ea2467382ceb9c9edb39b647390
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-04-10 14:59:16 +0200

    Properly integrating data augmentation function for training

--- SI_Toolkit_ASF/data_augmentation.py
+++ SI_Toolkit_ASF/data_augmentation.py
@@ -1,14 +1,12 @@
 from tqdm import trange
 from time import sleep
 
-AUGMENT_DATA = False
-
 
 def augment_data(data, labels):
 
-    if AUGMENT_DATA:
-        print('Augmenting data...')
-        sleep(0.002)
-        for i in trange(len(data)):
-            data[i] = data[i]
+    print('Augmenting data...')
+    sleep(0.002)
+    for i in trange(len(data)):
+        data[i] = data[i]
+
     return data, labels
