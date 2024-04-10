9cf7ed7ec45c702ad3e804e28c0496b77bb1f105
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-04-10 14:43:07 +0200

    Comment

--- SI_Toolkit_ASF/brunton_widget_extensions.py
+++ SI_Toolkit_ASF/brunton_widget_extensions.py
@@ -1,3 +1,9 @@
+"""
+Adds application specific additions to brunton GUI.
+get_feature_label allows replacing feature names with more descriptive labels.
+convert_units_inplace converts units of the data to the desired units for display only.
+All changes are done for GUI only, no changes to underlying dataset.
+"""
 import numpy as np
 
 def get_feature_label(feature):
