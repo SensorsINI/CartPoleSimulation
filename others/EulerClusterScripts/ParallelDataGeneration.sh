bd4bbe702d438645494b873b047321e74f80580d
Marcin Paluch Euler <marcin.paluch1994@gmail.com>
2024-07-10 11:04:31 +0200

    Changes to script for data generation on eurler cluster - just to call it with one longer command without cd each time.

--- others/EulerClusterScripts/ParallelDataGeneration.sh
+++ others/EulerClusterScripts/ParallelDataGeneration.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#SBATCH --array=1-2             # Create an array job with task IDs from 1 to 12
+#SBATCH --array=25-48             # Create an array job with task IDs from 1 to 12
 #SBATCH --cpus-per-task=1        # Assign the required number of CPUs per task
 #SBATCH --time=8:00:00           # Set the maximum job time
+#SBATCH --output=./others/EulerClusterScripts/slurm-%A_%a.out   # Output file
+
+# Create output and error directories if they do not exist
+mkdir -p ./others/EulerClusterScripts
 
 source $HOME/miniconda3/bin/activate
 conda activate CPS39
@@ -9,6 +13,5 @@ conda activate CPS39
 # Use SLURM_ARRAY_TASK_ID for the model index directly since it ranges from 1 to 50
 i=$SLURM_ARRAY_TASK_ID
 
-cd ../../
 # Run the Python script with the specific index
 python run_data_generator.py -i $i
