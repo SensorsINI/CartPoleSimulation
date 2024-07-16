2b8c3422f92d422ddc3545cdeb740e8cb7e080a0
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-07-16 18:06:57 +0200

    More changes to run control along trajectories on Euler.

--- SI_Toolkit_ASF/Run/PreprocessData_Add_Control_Along_Trajectories.py
+++ SI_Toolkit_ASF/Run/PreprocessData_Add_Control_Along_Trajectories.py
@@ -15,7 +15,33 @@ import numpy as np
 from CartPole.state_utilities import STATE_VARIABLES
 from others.globals_and_utils import MockSpace
 
-get_files_from = 'SI_Toolkit_ASF/Experiments/Trial_4_nt/test/'
+import argparse
+
+def args_fun():
+    parser = argparse.ArgumentParser(description='Generate CartPole data.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--secondary_experiment_index', default=-1, type=int,
+                        help='Additional index to the experiment folder (ML Pipeline mode) or file (otherwise) name. -1 to skip.')
+
+
+    args = parser.parse_args()
+
+    if args.secondary_experiment_index == -1:
+        args.secondary_experiment_index = None
+
+    digits = 3
+    if args.secondary_experiment_index is not None:
+        formatted_index = f"{args.secondary_experiment_index:0{digits}d}"
+    else:
+        formatted_index = None
+
+    return formatted_index
+
+formatted_index = args_fun()
+if formatted_index is not None:
+    get_files_from = f'SI_Toolkit_ASF/Experiments/Trial_4_nt/test/Experiment-{formatted_index}.csv'
+else:
+    get_files_from = 'SI_Toolkit_ASF/Experiments/Trial_4_nt/test/'
+
 save_files_to = 'SI_Toolkit_ASF/Experiments/Trial_4_nt/test_done/'
 
 controller = {
