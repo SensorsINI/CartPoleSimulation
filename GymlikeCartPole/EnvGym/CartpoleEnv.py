Fixing angle rotation rendering.

GymlikeCartPole/EnvGym/CartpoleEnv.py
@@ -152,7 +152,9 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
         pole_coords = []
         for coord in [(l, b), (l, t), (r, t), (r, b)]:
-            coord = pygame.math.Vector2(coord).rotate_rad(-self.state[ANGLE_IDX])
+            coord = pygame.math.Vector2(coord).rotate_rad(
+                self.cartpole_rl.angle_rotation_direction_rendering * self.state[ANGLE_IDX]
+            )
             coord = (coord[0] + cartx, coord[1] + carty + axleoffset)
             pole_coords.append(coord)
         gfxdraw.aapolygon(self.surf, pole_coords, (202, 152, 101))