c813b05482372f6fab7abf7886670252170e57d8
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-12-01 14:06:51 +0100

    PCA plot

--- SI_Toolkit_ASF/L4DC_Plots/PCA/PCA_plot.py
+++ SI_Toolkit_ASF/L4DC_Plots/PCA/PCA_plot.py
@@ -6,22 +6,19 @@ import matplotlib.gridspec as gridspec
 # 1. Set font sizes (same as the first plot)
 plt.rcParams.update({'font.size': 14})
 
+# Load the data
 df = pd.read_csv('PCA_components.csv')
-
-df = df.iloc[:-1500, :]
-L = df['L']*100
-
+df = df.iloc[:-1500, :]  # Slice the data
+L = df['L'] * 100
 PCA1 = df['PCA1']
 PCA2 = df['PCA2']
 time = df['time']
 time = time - time.min()
 
-fig = plt.figure(figsize=(16, 8))
+fig = plt.figure(figsize=(16, 4))
 
-# Define GridSpec with 3 rows and 2 columns
-# The second column is reserved for colorbars with a fixed width
-gs = gridspec.GridSpec(1, 4, width_ratios=[1, 0.05, 1, 0.05], height_ratios=[1],
-                       wspace=0.3, hspace=0.4)
+# Define GridSpec with 2 rows and 2 columns
+gs = gridspec.GridSpec(2, 2, height_ratios=[1, 0.05], hspace=0.7, wspace=0.2)
 
 # -----------------------------
 # First Subplot: PCA1 vs L
@@ -31,26 +28,27 @@ ax0.set_xlabel('Pole Length (cm)')
 ax0.set_ylabel('PCA1')
 ax0.grid(True)
 
-# Add colorbar for the first scatter plot
-cax0 = fig.add_subplot(gs[0, 1])
-cbar1 = fig.colorbar(scatter1, cax=cax0)
-cbar1.set_label('time')
+# Add colorbar for the first scatter plot below the plot
+cax0 = fig.add_subplot(gs[1, 0])
+cbar1 = fig.colorbar(scatter1, cax=cax0, orientation='horizontal')
+cbar1.set_label('Time (s)')
 
 # -----------------------------
 # Second Subplot: PCA1 vs time
-ax1 = fig.add_subplot(gs[0, 2])
+ax1 = fig.add_subplot(gs[0, 1])
 scatter2 = ax1.scatter(time, PCA1, c=L, cmap='coolwarm', alpha=0.7)
 ax1.set_xlabel('Time (s)')
 ax1.set_ylabel('PCA1')
 ax1.grid(True)
 
-# Add colorbar for the second scatter plot
-cax1 = fig.add_subplot(gs[0, 3])
-cbar2 = fig.colorbar(scatter2, cax=cax1)
+# Add colorbar for the second scatter plot below the plot
+cax1 = fig.add_subplot(gs[1, 1])
+cbar2 = fig.colorbar(scatter2, cax=cax1, orientation='horizontal')
 cbar2.set_label('Pole Length (cm)')
 
 # Adjust layout to prevent overlap
 plt.tight_layout()
+plt.savefig('PCA1.pdf', format='pdf', bbox_inches='tight', bbox_extra_artists=[cax1])
 
 # Display the plots
 plt.show()
