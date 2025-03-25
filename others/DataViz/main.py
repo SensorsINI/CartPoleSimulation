9f107920d3abb8851422601276c12a608fea156a
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-03-25 15:03:50 +0100

    DataViz v2

---
+++ others/DataViz/main.py
@@ -0,0 +1,23 @@
+# main.py
+
+import tkinter as tk
+from config import Config
+from data_loader import DataLoader
+from data_processor import DataProcessor
+from main_app import MainApplication
+
+
+def main():
+    config = Config()
+    loader = DataLoader(config)
+    df = loader.load_data()
+
+    processor = DataProcessor(config)
+    df = processor.compute_errors(df)
+
+    app = MainApplication(config, df)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
