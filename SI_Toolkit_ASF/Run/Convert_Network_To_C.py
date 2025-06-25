cd98cb8b40b091571f54c5ace51ea1d297fb35db
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-06-24 17:50:48 -0700

    First Adaptive network in C format added.

--- SI_Toolkit_ASF/Run/Convert_Network_To_C.py
+++ SI_Toolkit_ASF/Run/Convert_Network_To_C.py
@@ -3,8 +3,8 @@
 """ Dense, GRU and LSTM networks with tanh activation on all but last layer are supported! """
 from SI_Toolkit.C_implementation.TF2C import tf2C
 
-path_to_models = '../Experiments/Trial_14__17_08_2024/Models/'
-net_name = 'Dense-7IN-32H1-32H2-1OUT-0'
+path_to_models = '../Experiments/CP_Models_02_06_2025/Long/quant/'
+net_name = 'LSTM-7IN-64H1-64H2-1OUT-0'
 batch_size = 1
 
 if __name__ == '__main__':
