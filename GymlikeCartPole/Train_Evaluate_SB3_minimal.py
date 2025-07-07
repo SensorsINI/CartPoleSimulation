67dd878227bdbe6c6ac8bbb9369292174f4d7f56
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-07-06 23:57:36 -0600

    Former RL training script now as minimal training example

--- GymlikeCartPole/Train_Evaluate_SB3_minimal.py
+++ GymlikeCartPole/Train_Evaluate_SB3_minimal.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
@@ -11,14 +11,22 @@ import numpy as np
 # env_name = "MountainCarContinuous-v0" # Not working
 # env_name = "Pendulum-v1"
 # env_name = "CartPole-v0"
-# env = gym.make(env_name)
-
-
 # env = gym.make(env_name, render_mode=None)
+
+TRAINING_TYPE = "ppo"  # or "sac"
+
 env = CartPoleEnv(render_mode=None)
+
 env = DummyVecEnv([lambda: env])
-model = PPO('MlpPolicy', env, verbose=1)
+
+if TRAINING_TYPE == "ppo":
+    model = PPO('MlpPolicy', env, verbose=1)
+elif TRAINING_TYPE == "sac":
+    model = SAC('MlpPolicy', env, verbose=1)
+else:
+    raise ValueError("Unsupported training type. Use 'ppo' or 'sac'.")
 
 model.learn(total_timesteps=500000, progress_bar=True)
 
-model.save('ppo model')
+model.save(f"{TRAINING_TYPE} model")
+
