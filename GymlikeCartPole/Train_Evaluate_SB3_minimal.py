3995c3a31b6787442eb6e6589bff031288fbb05e
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-07-07 00:30:51 -0600

    Former RL training script now as minimal training and evaluation example

--- GymlikeCartPole/Train_Evaluate_SB3_minimal.py
+++ GymlikeCartPole/Train_Evaluate_SB3_minimal.py
@@ -8,16 +8,22 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-# env_name = "MountainCarContinuous-v0" # Not working
-# env_name = "Pendulum-v1"
-# env_name = "CartPole-v0"
-# env = gym.make(env_name, render_mode=None)
-
 TRAINING_TYPE = "ppo"  # or "sac"
 
-env = CartPoleEnv(render_mode=None)
 
-env = DummyVecEnv([lambda: env])
+def make_env(render_mode=None):
+
+    # env_name = "MountainCarContinuous-v0" # Not working
+    # env_name = "Pendulum-v1"
+    # env_name = "CartPole-v0"
+    # env = gym.make(env_name, render_mode=render_mode)
+
+    new_env = CartPoleEnv(render_mode=render_mode)
+
+    return new_env
+
+
+env = DummyVecEnv([lambda: make_env(render_mode=None)])
 
 if TRAINING_TYPE == "ppo":
     model = PPO('MlpPolicy', env, verbose=1)
@@ -30,3 +36,40 @@ model.learn(total_timesteps=500000, progress_bar=True)
 
 model.save(f"{TRAINING_TYPE} model")
 
+
+
+# VISUAL EVALUATION
+env = DummyVecEnv([lambda: make_env(render_mode="human")])
+evaluate_policy(model, env, n_eval_episodes=2, render=True)
+env.close()
+
+# EVALUATION WITHOUT RENDERING
+env = DummyVecEnv([lambda: make_env(render_mode=None)])
+
+for episode in range(1, 3):
+    score = 0
+    action_buf = []
+    obs_buf = []
+    obs = env.reset()
+    done = False
+
+    while not done:
+        env.render()
+        action, _ = model.predict(obs)
+        action_buf.append(action)
+        obs, reward, done, info = env.step(action)
+        obs_buf.append(obs)
+        score += reward
+
+    print('Episode:', episode, 'Score:', score)
+
+    action_buf = np.stack(action_buf)
+    action_buf = np.squeeze(action_buf)
+    plt.figure()
+    plt.plot(action_buf)
+    plt.title('Actions of RL agent')
+    plt.xlabel('Timesteps')
+    plt.ylabel('Action')
+    plt.show()
+
+env.close()
